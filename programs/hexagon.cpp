abbf4ea69ae92c8db3563864f042d8a83acda9b6
Mark Oates <mark.oates@shopify.com>
2018-09-22 22:21:52 -0400

    Prevent regex from trimming intentional whitespace

--- programs/hexagon.cpp
+++ programs/hexagon.cpp
@@ -1235,7 +1235,7 @@ public:
       std::vector<std::string> regex_input_file_lines;
       if (!read_file(regex_input_file_lines, REGEX_TEMP_FILENAME) || regex_input_file_lines.size() == 0) throw std::runtime_error("cannot open expected REGEX_TEMP_FILENAME file for input, or is empty");
 
-      std::string regex_expression = php::trim(regex_input_file_lines[0]);
+      std::string regex_expression = regex_input_file_lines[0];
       std::size_t carat_position = regex_expression.find_last_of('^');
       int cursor_placement_offset = (carat_position == std::string::npos) ? 0 : carat_position;
       regex_expression.erase(std::remove(regex_expression.begin() + 1, regex_expression.end(), '^'), regex_expression.end());
