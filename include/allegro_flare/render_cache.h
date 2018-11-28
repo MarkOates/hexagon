8339446de4b6b8d6962b59b1e4e6bd742e889454
Mark Oates <mark.oates@shopify.com>
2018-11-27 20:39:58 -0500

    Add RenderCache from allegro_flare

---
+++ include/allegro_flare/render_cache.h
@@ -0,0 +1,37 @@
+#ifndef __AF_RENDER_CACHE_HEADER
+#define __AF_RENDER_CACHE_HEADER
+
+
+
+
+#define ALLEGRO_UNSTABLE
+
+#include <allegro5/allegro.h>
+
+
+
+
+class RenderCache
+{
+private:
+   ALLEGRO_BITMAP *render;
+   int padding;
+   ALLEGRO_STATE previous_state;
+   int previous_samples; // should not be necessairy in future versions of Allegro
+   int previous_depth;
+   bool dirty;
+
+public:
+   RenderCache();
+   void setup_surface(int w, int h, int multisamples=0, int depth=0);
+   void finish_surface();
+   void draw(float origin_x, float origin_y);
+   void mark_as_dirty();
+   void mark_as_clean();
+   bool is_dirty();
+};
+
+
+
+
+#endif
