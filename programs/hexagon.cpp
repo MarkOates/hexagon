40bb1775bc59a631357bddf9b2fe1494afa557e0
Mark Oates <markoates0@gmail.com>
2019-08-17 01:38:37 -0400

    Remove KeyboardInputsModal inlined class def

--- programs/hexagon.cpp
+++ programs/hexagon.cpp
@@ -355,44 +355,7 @@ public:
 #include <Hexagon/StageInterface.hpp>
 
 
-class KeyboardInputsModal : public StageInterface
-{
-private:
-   KeyboardCommandMapper *keyboard_command_mapper;
-
-public:
-   KeyboardInputsModal(KeyboardCommandMapper *keyboard_command_mapper)
-      : StageInterface(KEYBOARD_INPUTS_MODAL)
-      , keyboard_command_mapper(keyboard_command_mapper)
-   {}
-
-   virtual void render(ALLEGRO_DISPLAY *display, ALLEGRO_FONT *font, int cell_width, int cell_height)
-   {
-      if (!display) throw std::runtime_error("[KeyboardInputsModal] error: display cannot be a nullptr.");
-      if (!font) throw std::runtime_error("[KeyboardInputsModal] error: font cannot be a nullptr.");
-
-      placement3d &place = get_place();
-
-      float border_thickness = 5.0;
-      place.size = vec3d(500.0, 400.0, 0.0);
-      place.align = vec3d(0.5, 0.5, 0.0);
-      place.rotation = vec3d(place.rotation.x, place.rotation.y, place.rotation.z);
-
-      place.start_transform();
-      al_draw_filled_rectangle(0, 0, place.size.x, place.size.y, color::black);
-      al_draw_rounded_rectangle(0 - border_thickness, 0 - border_thickness, place.size.x + border_thickness, place.size.y + border_thickness, 8.0, 8.0, color::dimgray, border_thickness);
-      place.restore_transform();
-   }
-
-   virtual void process_local_event(std::string event_name, ActionData action_data=ActionData()) {}
-   virtual void process_event(ALLEGRO_EVENT &event) {}
-   virtual bool save_file() { return true; }
-};
-
-
-
-#include <string>
-#include <vector>
+#include <Hexagon/KeyboardInputsModal.hpp>
 
 
 
