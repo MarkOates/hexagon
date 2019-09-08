af91a1a79711b7f7c27d75fc9bdc3bbe21f665f2
Mark Oates <markoates0@gmail.com>
2019-09-08 17:39:27 -0400

    Move "RailsMinitestTestRunner" to hpp file

---
+++ include/Hexagon/RailsMinitestTestRunner.hpp
@@ -0,0 +1,29 @@
+
+#pragma once
+
+class RailsMinitestTestRunner
+{
+private:
+   std::string test_filename;
+
+public:
+   RailsMinitestTestRunner(std::string test_filename)
+      : test_filename(test_filename)
+   {}
+   ~RailsMinitestTestRunner() {}
+
+   std::string get_execution_command()
+   {
+      std::stringstream test_command_string;
+      test_command_string << "bin/rails test " << test_filename << " -v";
+      return test_command_string.str();
+   }
+
+   std::string run()
+   {
+      ShellCommandExecutor shell_command_executor(get_execution_command());
+      std::string output = shell_command_executor.execute(command_line_exec_callback);
+      return output;
+   }
+};
+
--- programs/hexagon.cpp
+++ programs/hexagon.cpp
@@ -195,32 +195,7 @@ void command_line_exec_callback(std::string new_content)
 
 
 
-class RailsMinitestTestRunner
-{
-private:
-   std::string test_filename;
-
-public:
-   RailsMinitestTestRunner(std::string test_filename)
-      : test_filename(test_filename)
-   {}
-   ~RailsMinitestTestRunner() {}
-
-   std::string get_execution_command()
-   {
-      std::stringstream test_command_string;
-      test_command_string << "bin/rails test " << test_filename << " -v";
-      return test_command_string.str();
-   }
-
-   std::string run()
-   {
-      ShellCommandExecutor shell_command_executor(get_execution_command());
-      std::string output = shell_command_executor.execute(command_line_exec_callback);
-      return output;
-   }
-};
-
+#include <Hexagon/RailsMinitestTestRunner.hpp>
 
 
 
