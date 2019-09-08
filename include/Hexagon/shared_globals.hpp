80fe1bd06305e89874c562b3fdb307ce9bc62caf
Mark Oates <markoates0@gmail.com>
2019-09-07 22:44:29 -0400

    Add/create "MAKE_COMMAND_FILENAME"

--- include/Hexagon/shared_globals.hpp
+++ include/Hexagon/shared_globals.hpp
@@ -10,6 +10,7 @@ extern std::string REGEX_TEMP_FILENAME;
 extern std::string CLIPBOARD_TEMP_FILENAME;
 extern std::string FILE_NAVIGATOR_SELECTION_FILENAME;
 extern std::string FILE_NAVIGATOR_SELECTION_last_content;
+extern std::string MAKE_COMMAND_FILENAME;
 
 
 
--- src/Hexagon/shared_globals.cpp
+++ src/Hexagon/shared_globals.cpp
@@ -9,6 +9,7 @@ std::string REGEX_TEMP_FILENAME = "regex.txt";
 std::string CLIPBOARD_TEMP_FILENAME = "clipboard.txt";
 std::string FILE_NAVIGATOR_SELECTION_FILENAME = "file_navigator_selection.txt";
 std::string FILE_NAVIGATOR_SELECTION_last_content = "";
+std::string MAKE_COMMAND_FILENAME = "make_command.txt";
 
 
 
