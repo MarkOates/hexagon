7ade3a44c63c6aecb69a53b553f05ce21aaf144b
Mark Oates <markoates0@gmail.com>
2019-09-22 15:36:22 -0400

    cg FilesystemNode

---
+++ include/Hexagon/FileNavigator/FileystemNode.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace FileNavigator
+   {
+      class FileystemNode
+      {
+      private:
+
+      public:
+         FileystemNode();
+         ~FileystemNode();
+
+
+      std::string run();
+      };
+   }
+}
+
+
+
---
+++ src/Hexagon/FileNavigator/FileystemNode.cpp
@@ -0,0 +1,30 @@
+
+
+#include <Hexagon/FileNavigator/FileystemNode.hpp>
+
+
+
+namespace Hexagon
+{
+namespace FileNavigator
+{
+
+
+FileystemNode::FileystemNode()
+{
+}
+
+
+FileystemNode::~FileystemNode()
+{
+}
+
+
+std::string FileystemNode::run()
+{
+return "Hello World!";
+}
+} // namespace FileNavigator
+} // namespace Hexagon
+
+
