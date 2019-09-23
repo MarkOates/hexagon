1ad8a58f989a8a32e52e40cd744c3fadd346d3d2
Mark Oates <markoates0@gmail.com>
2019-09-22 21:18:04 -0400

    Uncomment some lines in FileNavigator::StageTest

--- tests/Hexagon/FileNavigator/StageTest.cpp
+++ tests/Hexagon/FileNavigator/StageTest.cpp
@@ -6,6 +6,6 @@
 TEST(Hexagon_FileNavigator_StageTest, run__returns_the_expected_response)
 {
    Hexagon::FileNavigator::Stage program_runner;
-   //std::string expected_string = "Hello World!";
-   //EXPECT_EQ(expected_string, program_runner.run());
+   std::string expected_string = "Hello World!";
+   EXPECT_EQ(expected_string, program_runner.run());
 }
