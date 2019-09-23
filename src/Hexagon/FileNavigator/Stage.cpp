79e690543bcb5949fb0b1383c188494986ce507e
Mark Oates <markoates0@gmail.com>
2019-09-22 21:45:05 -0400

    Add some impl to "process_local_event" on FileNavigator::Stage

--- src/Hexagon/FileNavigator/Stage.cpp
+++ src/Hexagon/FileNavigator/Stage.cpp
@@ -1,7 +1,11 @@
 
 
 #include <Hexagon/FileNavigator/Stage.hpp>
-
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_primitives.h>
+#include <iostream>
+#include <iostream>
+#include <iostream>
 
 
 namespace Hexagon
@@ -37,13 +41,28 @@ return "Hello World!";
 
 void Stage::render(ALLEGRO_DISPLAY* display, ALLEGRO_FONT* font, int cell_width, int cell_height)
 {
+al_draw_filled_circle(100, 100, 60, al_color_name("green"));
 return;
 
 }
 
 void Stage::process_local_event(std::string event_name, ActionData action_data)
 {
-return;
+std::cout << "LocalEvent::" << event_name << std::endl;
+
+try
+{
+   bool executed = false;
+
+   //if (event_name == ROTATE_STAGE_RIGHT) { executed = true; rotate_stage_right(); }
+   //else if (event_name == ROTATE_STAGE_LEFT) { executed = true; rotate_stage_left(); }
+
+   if (!executed) std::cerr << "???? cannot execute \"" << event_name << "\".  It does not exist." << std::endl;
+}
+catch (const std::exception &exception)
+{
+   std::cerr << ">BOOM< cannot execute \"" << event_name << "\".  The following exception occurred: " << exception.what() << std::endl;
+}
 
 }
 
