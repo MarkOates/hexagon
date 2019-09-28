22412ffa22b5f85b2aa5ae128da62a040862932a
Mark Oates <markoates0@gmail.com>
2019-09-28 14:23:45 -0400

    SPAWN_FILE_NAVIGATOR_IF_NO_STAGES_EXIST after ESCAPE_CURRENT_MODAL

--- programs/hexagon.cpp
+++ programs/hexagon.cpp
@@ -852,8 +852,8 @@ public:
       keyboard_command_mapper.set_mapping(ALLEGRO_KEY_ESCAPE, true, false, false, false, { DESTROY_TOPMOST_STAGE, SPAWN_FILE_NAVIGATOR_IF_NO_STAGES_EXIST });
       if (is_current_stage_a_modal())
       {
-         //keyboard_command_mapper.set_mapping(ALLEGRO_KEY_Q, false, false, false, false, { ESCAPE_CURRENT_MODAL });
-         keyboard_command_mapper.set_mapping(ALLEGRO_KEY_ESCAPE, false, false, false, false, { ESCAPE_CURRENT_MODAL });
+         //keyboard_command_mapper.set_mapping(ALLEGRO_KEY_Q, false, false, false, false, { ESCAPE_CURRENT_MODAL, SPAWN_FILE_NAVIGATOR_IF_NO_STAGES_EXIST });
+         keyboard_command_mapper.set_mapping(ALLEGRO_KEY_ESCAPE, false, false, false, false, { ESCAPE_CURRENT_MODAL, SPAWN_FILE_NAVIGATOR_IF_NO_STAGES_EXIST });
          if (is_current_stage_a_regex_input_box())
          {
            keyboard_command_mapper.set_mapping(ALLEGRO_KEY_ENTER, false, false, false, false, { SUBMIT_CURRENT_MODAL });
