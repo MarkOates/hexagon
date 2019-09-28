2bd7df0e2de5b515f3e77dd1df059e8067143c07
Mark Oates <markoates0@gmail.com>
2019-09-28 15:43:00 -0400

    Add impl for ComponentNavigator/List::components()

--- src/Hexagon/ComponentNavigator/List.cpp
+++ src/Hexagon/ComponentNavigator/List.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include <Blast/ShellCommandExecutorWithCallback.hpp>
+#include <NcursesArt/ProjectComponentBasenameExtractor.hpp>
+#include <Blast/StringSplitter.hpp>
 
 
 namespace Hexagon
@@ -26,9 +28,20 @@ List::~List()
 std::vector<std::string> List::components()
 {
 std::vector<std::string> result = {};
+
 std::string find_command = "find quintessence -type f -name \"*.q.yml\"";
 Blast::ShellCommandExecutorWithCallback executor(find_command);
-result = { executor.execute() };
+std::string executor_response = executor.execute();
+
+Blast::StringSplitter splitter(executor_response, '\n');
+std::vector<std::string> quintessence_filenames = splitter.split();
+
+for (auto &item : quintessence_filenames)
+{
+  NcursesArt::ProjectComponentBasenameExtractor extractor(item);
+  result.push_back(extractor.identify_component_basename());
+}
+
 return result;
 
 }
--- tests/Hexagon/ComponentNavigator/ListTest.cpp
+++ tests/Hexagon/ComponentNavigator/ListTest.cpp
@@ -7,7 +7,18 @@ TEST(Hexagon_ComponentNavigator_ListTest, run__returns_the_expected_response)
 {
    Hexagon::ComponentNavigator::List list;
    std::vector<std::string> expected_list = {
-     { "Hello World!" },
+      "Hexagon/RerunOutputWatcher",
+      "Hexagon/RailsTestOutputParser",
+      "Hexagon/LayoutPlacements",
+      "Hexagon/Logo",
+      "Hexagon/RegexStore",
+      "Hexagon/ComponentNavigator/List",
+      "Hexagon/FileNavigator/Stage",
+      "Hexagon/FileNavigator/FileystemNode",
+      "Hexagon/LayoutFromYAML",
+      "NcursesArt/ProjectComponentBasenameExtractor",
+      "Blast/ShellCommandExecutorWithCallback",
    };
+
    EXPECT_EQ(expected_list, list.components());
 }
