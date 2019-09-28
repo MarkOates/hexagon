7e4a640ce4a1bfb9dad0d9569f6182b19628057c
Mark Oates <markoates0@gmail.com>
2019-09-28 10:47:33 -0400

    Add test for "RegexStore.class_names()"

--- tests/Hexagon/RegexStoreTest.cpp
+++ tests/Hexagon/RegexStoreTest.cpp
@@ -9,3 +9,11 @@ TEST(Hexagon_RegexStoreTest, identifiers__returns_the_expected_regex_string)
    std::string expected_string = "[a-z_][0-9a-z_]{0,31}";
    EXPECT_EQ(expected_string, regex_store.identifiers());
 }
+
+TEST(Hexagon_RegexStoreTest, class_names_returns_the_expected_regex_string)
+{
+   Hexagon::RegexStore regex_store;
+   std::string expected_string = "[A-Z][0-9A-Za-z_]{0,31}";
+   EXPECT_EQ(expected_string, regex_store.());
+}
+
