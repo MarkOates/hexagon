45219a42a6cecd22836dd40b90f3f630eccad15b
Mark Oates <markoates0@gmail.com>
2019-10-10 00:23:25 -0400

    Modify "threads" var to include the data

--- programs/thread_experiment.cpp
+++ programs/thread_experiment.cpp
@@ -10,20 +10,19 @@ static void *main_thread_func(ALLEGRO_THREAD *thread, void *arg)
 
 int main(int argc, char **argv)
 {
-   std::vector<ALLEGRO_THREAD *> threads;
+   std::vector<std::pair<ALLEGRO_THREAD *, void *(*)(ALLEGRO_THREAD*, void*)>> threads = {};
 
    al_init();
-   ALLEGRO_THREAD *main_thread = al_create_thread(main_thread_func, nullptr);
 
    int num_threads = 2;
    int i;
 
-   for (auto &thread : threads) thread = al_create_thread(main_thread_func, NULL);
-   for (auto &thread : threads) al_start_thread(thread);
+   for (auto &thread : threads) thread.first = al_create_thread(main_thread_func, NULL);
+   for (auto &thread : threads) al_start_thread(thread.first);
    for (auto &thread : threads)
    {
-      al_join_thread(thread, NULL);
-      al_destroy_thread(thread);
+      al_join_thread(thread.first, NULL);
+      al_destroy_thread(thread.first);
    }
 }
 
