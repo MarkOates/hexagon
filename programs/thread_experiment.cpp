4c3cad70a55149a411cc6474de3fee8516f00a68
Mark Oates <markoates0@gmail.com>
2019-10-10 00:28:02 -0400

    Move complex pair type into thread_type

--- programs/thread_experiment.cpp
+++ programs/thread_experiment.cpp
@@ -8,21 +8,27 @@ static void *main_thread_func(ALLEGRO_THREAD *thread, void *arg)
    std::cout << "ABCdefghi" << std::endl;
 }
 
+struct thread_type {
+   ALLEGRO_THREAD *thread;
+   void *(*function)(ALLEGRO_THREAD*, void*);
+};
+
 int main(int argc, char **argv)
 {
-   std::vector<std::pair<ALLEGRO_THREAD *, void *(*)(ALLEGRO_THREAD*, void*)>> threads = {};
+   std::vector<thread_type> threads = {};
 
    al_init();
 
    int num_threads = 2;
    int i;
 
-   for (auto &thread : threads) thread.first = al_create_thread(main_thread_func, NULL);
-   for (auto &thread : threads) al_start_thread(thread.first);
+   for (auto &thread : threads) thread.thread = al_create_thread(thread.function, NULL);
+   for (auto &thread : threads) al_start_thread(thread.thread);
    for (auto &thread : threads)
    {
-      al_join_thread(thread.first, NULL);
-      al_destroy_thread(thread.first);
+      al_join_thread(thread.thread, NULL);
+      al_destroy_thread(thread.thread);
+      thread.thread = nullptr;
    }
 }
 
