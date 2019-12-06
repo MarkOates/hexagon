fc926474d2c26f2b058bbcd4a72a90247a2ef5c2
Mark Oates <markoates0@gmail.com>
2019-12-06 17:30:37 -0500

    Add symlink built "Blast/FileExistenceChecker"

---
+++ src/Blast/FileExistenceChecker.cpp
@@ -0,0 +1,35 @@
+
+
+#include <Blast/FileExistenceChecker.hpp>
+#include <filesystem>
+
+
+namespace Blast
+{
+
+
+FileExistenceChecker::FileExistenceChecker(std::string filename)
+   : filename(filename)
+{
+}
+
+
+FileExistenceChecker::~FileExistenceChecker()
+{
+}
+
+
+std::string FileExistenceChecker::get_filename()
+{
+   return filename;
+}
+
+
+bool FileExistenceChecker::exists()
+{
+return std::filesystem::exists(filename);
+
+}
+} // namespace Blast
+
+
