32cde7420c786ba2c6487b5f0227b4db71aa3730
Mark Oates <markoates0@gmail.com>
2019-12-08 03:22:33 -0500

    Fix non-compiling RerunOutputWatcher/Stage test

--- tests/Hexagon/RerunOutputWatcher/StageTest.cpp
+++ tests/Hexagon/RerunOutputWatcher/StageTest.cpp
@@ -1,11 +1,11 @@
 
 #include <gtest/gtest.h>
 
-#include <Hexagon/RerunOutputWatcher.hpp>
+#include <Hexagon/RerunOutputWatcher/Stage.hpp>
 
-TEST(Hexagon_RerunOutputWatcherTest, run__returns_the_expected_response)
+TEST(Hexagon_RerunOutputWatcher_StageTest, run__returns_the_expected_response)
 {
-   Hexagon::RerunOutputWatcher program_runner;
+   Hexagon::RerunOutputWatcher::Stage program_runner;
    std::string expected_string = "Hello World!";
    //EXPECT_EQ(expected_string, program_runner.run());
 }
