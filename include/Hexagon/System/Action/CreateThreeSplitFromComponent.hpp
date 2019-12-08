f514bf50ec0d1d074105beadd6456683ef29a206
Mark Oates <markoates0@gmail.com>
2019-12-08 03:59:27 -0500

    Expand ThreeSplit

--- include/Hexagon/System/Action/CreateThreeSplitFromComponent.hpp
+++ include/Hexagon/System/Action/CreateThreeSplitFromComponent.hpp
@@ -4,7 +4,6 @@
 #include <Blast/Project/Component.hpp>
 #include <Hexagon/Action.hpp>
 #include <Hexagon/StageInterface.hpp>
-#include <allegro_flare/placement3d.h>
 #include <string>
 #include <vector>
 
@@ -18,6 +17,7 @@ namespace Hexagon
          class CreateThreeSplitFromComponent : public ::Action
          {
          private:
+            std::string project_path;
             Blast::Project::Component component;
             std::vector<StageInterface *>& stages;
             int display_default_width;
@@ -25,14 +25,18 @@ namespace Hexagon
             static std::vector<StageInterface *> dummy_stages;
 
          public:
-            CreateThreeSplitFromComponent(std::vector<StageInterface *>& stages=get_dummy_stages_ref(), int display_default_width=2430, int display_default_height=1350);
+            CreateThreeSplitFromComponent(std::string project_path="/Users/markoates/Repos/hexagon/", std::vector<StageInterface *>& stages=get_dummy_stages_ref(), int display_default_width=2430, int display_default_height=1350);
             ~CreateThreeSplitFromComponent();
 
             void set_stages(std::vector<StageInterface *>& stages);
 
+            std::string get_project_path();
             std::vector<StageInterface *>& get_stages();
             static std::vector<StageInterface *> &get_dummy_stages_ref();
-         bool place_stage(std::string filename="/foobar.txt", std::string file_contents="/Hello World", placement3d place={});
+         bool place_stage(std::string filename="/foobar.txt", std::string file_contents="/Hello World", float x=0.0, float align_x=0.0);
+         std::string generate_header_filename();
+         std::string generate_source_filename();
+         std::string generate_test_filename();
          bool execute();
          };
       }
--- src/Hexagon/System/Action/CreateThreeSplitFromComponent.cpp
+++ src/Hexagon/System/Action/CreateThreeSplitFromComponent.cpp
@@ -1,7 +1,9 @@
 
 
 #include <Hexagon/System/Action/CreateThreeSplitFromComponent.hpp>
+#include <allegro_flare/placement3d.h>
 #include <Hexagon/CodeEditor/Stage.hpp>
+#include <Blast/ProjectComponentFilenameGenerator.hpp>
 
 
 namespace Hexagon
@@ -15,8 +17,9 @@ namespace Action
 std::vector<StageInterface *> CreateThreeSplitFromComponent::dummy_stages = {};
 
 
-CreateThreeSplitFromComponent::CreateThreeSplitFromComponent(std::vector<StageInterface *>& stages, int display_default_width, int display_default_height)
+CreateThreeSplitFromComponent::CreateThreeSplitFromComponent(std::string project_path, std::vector<StageInterface *>& stages, int display_default_width, int display_default_height)
    : ::Action("System::Action::CreateThreeSplitFromComponent", ActionData())
+   , project_path(project_path)
    , component()
    , stages(stages)
    , display_default_width(display_default_width)
@@ -36,6 +39,12 @@ void CreateThreeSplitFromComponent::set_stages(std::vector<StageInterface *>& st
 }
 
 
+std::string CreateThreeSplitFromComponent::get_project_path()
+{
+   return project_path;
+}
+
+
 std::vector<StageInterface *>& CreateThreeSplitFromComponent::get_stages()
 {
    return stages;
@@ -48,9 +57,17 @@ std::vector<StageInterface *> &CreateThreeSplitFromComponent::get_dummy_stages_r
 }
 
 
-bool CreateThreeSplitFromComponent::place_stage(std::string filename, std::string file_contents, placement3d place)
+bool CreateThreeSplitFromComponent::place_stage(std::string filename, std::string file_contents, float x, float align_x)
 {
 CodeEditor::Stage *file_stage = new CodeEditor::Stage(filename);
+
+float width = display_default_width/3;
+
+placement3d place(x, 0, 0);
+place.size = vec3d(width, display_default_height, 0.0); //al_get_display_width(display), al_get_display_height(display), 0.0);
+place.align = vec3d(align_x, 0.5, 0.0);
+place.scale = vec3d(0.9, 0.9, 0.0);
+
 file_stage->set_place(place);
 file_stage->set_content(file_contents);
 stages.push_back(file_stage);
@@ -59,10 +76,32 @@ return true;
 
 }
 
+std::string CreateThreeSplitFromComponent::generate_header_filename()
+{
+return project_path + Blast::ProjectComponentFilenameGenerator(component.get_name(), Blast::ProjectComponentFileTypes::HEADER_FILE).generate_filename();
+
+}
+
+std::string CreateThreeSplitFromComponent::generate_source_filename()
+{
+return project_path + Blast::ProjectComponentFilenameGenerator(component.get_name(), Blast::ProjectComponentFileTypes::SOURCE_FILE).generate_filename();
+
+}
+
+std::string CreateThreeSplitFromComponent::generate_test_filename()
+{
+return project_path + Blast::ProjectComponentFilenameGenerator(component.get_name(), Blast::ProjectComponentFileTypes::TEST_FILE).generate_filename();
+
+}
+
 bool CreateThreeSplitFromComponent::execute()
 {
 float left_anchor = -display_default_width / 3;
 float right_anchor = display_default_width / 3;
+
+std::string header_filename = generate_header_filename();
+std::string source_filename = generate_source_filename();
+std::string test_filename = generate_test_filename();
 
 return true;
 
