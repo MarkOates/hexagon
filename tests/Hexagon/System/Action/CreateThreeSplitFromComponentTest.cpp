2404ede1a846af8c6f92f0ac6273cbbbf3995b55
Mark Oates <markoates0@gmail.com>
2019-12-12 23:09:33 -0500

    Add additional test to Action/CreateThreeSplit

--- tests/Hexagon/System/Action/CreateThreeSplitFromComponentTest.cpp
+++ tests/Hexagon/System/Action/CreateThreeSplitFromComponentTest.cpp
@@ -20,3 +20,19 @@ TEST(Hexagon_System_Action_CreateThreeSplitFromComponentTest, execute__creates_t
    ASSERT_EQ(3, stages.size());
 }
 
+TEST(Hexagon_System_Action_CreateThreeSplitTest, execute__with_a_valid_component_creates_the_expected_files)
+{
+   std::string project_path = "Users/markoates/Repos/hexagon/";
+   std::string component = "Hexagon/Action";
+   std::vector<StageInterface *> stages;
+
+   CreateThreeSplit create_three_split(project_path, component, stages);
+
+   EXPECT_EQ(true, create_three_split.execute());
+
+   ASSERT_EQ(3, stages.size());
+   for (auto &stage : stages)
+   {
+      EXPECT_EQ(StageInterface::CODE_EDITOR, stage->get_type());
+   }
+}
