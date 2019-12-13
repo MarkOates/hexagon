f6dc8ca97d412c0711bfe84678ed6be93521e375
Mark Oates <markoates0@gmail.com>
2019-12-12 23:01:52 -0500

    Add CreateThreeSplit test

--- tests/Hexagon/System/Action/CreateThreeSplitFromComponentTest.cpp
+++ tests/Hexagon/System/Action/CreateThreeSplitFromComponentTest.cpp
@@ -3,6 +3,8 @@
 
 #include <Hexagon/System/Action/CreateThreeSplitFromComponent.hpp>
 
+typedef Hexagon::System::Action::CreateThreeSplitFromComponent CreateThreeSplit;
+
 TEST(Hexagon_System_Action_CreateThreeSplitFromComponentTest, can_be_created_without_blowing_up)
 {
    Hexagon::System::Action::CreateThreeSplitFromComponent create_three_split_from_component;
@@ -10,6 +12,11 @@ TEST(Hexagon_System_Action_CreateThreeSplitFromComponentTest, can_be_created_wit
 
 TEST(Hexagon_System_Action_CreateThreeSplitFromComponentTest, execute__creates_three_stages_split_across_the_width)
 {
-   Hexagon::System::Action::CreateThreeSplitFromComponent create_three_split_from_component;
-   EXPECT_EQ(true, create_three_split_from_component.execute());
+   std::string project_path = "Users/markoates/Repos/hexagon/";
+   std::string component = "Hexagon/Action";
+   std::vector<StageInterface *> stages;
+   CreateThreeSplit create_three_split(project_path, component, stages);
+   EXPECT_EQ(true, create_three_split.execute());
+   ASSERT_EQ(3, stages.size());
 }
+
