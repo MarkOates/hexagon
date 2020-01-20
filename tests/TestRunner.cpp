92d2cf138a15a5791970e212d9aab451c40401d9
Mark Oates <mark.oates@shopify.com>
2020-01-20 16:41:32 -0500

    Rename "test_runner.cpp" -> "TestRunner.cpp"; Add include allegro.h header

--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -1,8 +1,7 @@
 
-
+#include <allegro5/allegro.h>
 
 #include <gtest/gtest.h>
-
 
 
 int main(int argc, char **argv)
@@ -10,6 +9,4 @@ int main(int argc, char **argv)
    ::testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
 }
-
-
 
