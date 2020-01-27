92c463152d4d5c6c8fe86ff15b102f5027e6d7c1
Mark Oates <mark.oates@shopify.com>
2020-01-27 09:56:25 -0500

    Update generated source file for Blast/FileExistenceChecker

--- src/Blast/FileExistenceChecker.cpp
+++ src/Blast/FileExistenceChecker.cpp
@@ -30,7 +30,9 @@ std::string FileExistenceChecker::get_filename()
 bool FileExistenceChecker::exists()
 {
 struct stat info;
-return stat(filename.c_str(), &info) == 0 && (S_ISREG(info.st_mode) || S_ISLNK(info.st_mode));
+//disable for Windows compile
+//return stat(filename.c_str(), &info) == 0 && (S_ISREG(info.st_mode) || S_ISLNK(info.st_mode));
+return false;
 
 }
 } // namespace Blast
