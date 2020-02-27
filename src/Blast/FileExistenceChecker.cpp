7675be16c714ae888ef05716ea82c9091d50122a
Mark Oates <mark.oates@shopify.com>
2020-02-26 19:51:07 -0500

    Restore non-windows friendly implementation of Blast/FileExistenceChecker

--- src/Blast/FileExistenceChecker.cpp
+++ src/Blast/FileExistenceChecker.cpp
@@ -31,8 +31,8 @@ bool FileExistenceChecker::exists()
 {
 struct stat info;
 //disable for Windows compile
-//return stat(filename.c_str(), &info) == 0 && (S_ISREG(info.st_mode) || S_ISLNK(info.st_mode));
-return false;
+return stat(filename.c_str(), &info) == 0 && (S_ISREG(info.st_mode) || S_ISLNK(info.st_mode));
+//return false;
 
 }
 } // namespace Blast
