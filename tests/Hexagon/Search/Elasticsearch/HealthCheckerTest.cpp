bf5464336878e5d569c7f308623d07925a1d6664
Mark Oates <markoates0@gmail.com>
2020-03-26 19:32:55 -0400

    Fix "Blast" -> "Hexagon" namespace for Elasticsearch/HealthChecker

--- tests/Hexagon/Search/Elasticsearch/HealthCheckerTest.cpp
+++ tests/Hexagon/Search/Elasticsearch/HealthCheckerTest.cpp
@@ -4,17 +4,17 @@
 
 using ::testing::MatchesRegex;
 
-#include <Blast/Search/Elasticsearch/HealthChecker.hpp>
+#include <Hexagon/Search/Elasticsearch/HealthChecker.hpp>
 
-TEST(Blast_Search_Elasticsearch_HealthCheckerTest, can_be_created_without_blowing_up)
+TEST(Hexagon_Search_Elasticsearch_HealthCheckerTest, can_be_created_without_blowing_up)
 {
-   Blast::Search::Elasticsearch::HealthChecker health_checker;
+   Hexagon::Search::Elasticsearch::HealthChecker health_checker;
 }
 
-//TEST(Blast_Search_Elasticsearch_HealthCheckerTest,
+//TEST(Hexagon_Search_Elasticsearch_HealthCheckerTest,
 //   request_health__returns_the_health_of_the_elasticsearch_host)
 //{
-//   Blast::Search::Elasticsearch::HealthChecker health_checker;
+//   Hexagon::Search::Elasticsearch::HealthChecker health_checker;
 //
 //   std::string expected_matching_regex = "[0-9]+ [0-9]+:[0-9]+:[0-9]+ [0-0a-zA-Z_]+ (yellow|red|green) .+";
 //   std::string actual_health_string = health_checker.request_health();
@@ -22,16 +22,16 @@ TEST(Blast_Search_Elasticsearch_HealthCheckerTest, can_be_created_without_blowin
 //   EXPECT_THAT(actual_health_string, MatchesRegex(expected_matching_regex));
 //}
 //
-//TEST(Blast_Search_Elasticsearch_HealthCheckerTest,
+//TEST(Hexagon_Search_Elasticsearch_HealthCheckerTest,
 //   is_up__returns_true_if_the_elasticsearch_service_is_available)
 //{
-//   Blast::Search::Elasticsearch::HealthChecker health_checker;
+//   Hexagon::Search::Elasticsearch::HealthChecker health_checker;
 //   EXPECT_EQ(true, health_checker.is_up());
 //}
 //
-//TEST(Blast_Search_Elasticsearch_HealthCheckerTest,
+//TEST(Hexagon_Search_Elasticsearch_HealthCheckerTest,
 //   is_up__returns_false_if_the_elasticsearch_service_is_not_available)
 //{
-//   Blast::Search::Elasticsearch::HealthChecker health_checker;
+//   Hexagon::Search::Elasticsearch::HealthChecker health_checker;
 //   EXPECT_EQ(false, health_checker.is_up());
 //}
