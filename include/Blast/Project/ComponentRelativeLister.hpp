d392f2454af3bd48f5284c0b39f1893481aea841
Mark Oates <markoates0@gmail.com>
2020-03-28 01:39:54 -0400

    Add quintessence and generated files for Blast/Project/ComponentRelativeLister

---
+++ include/Blast/Project/ComponentRelativeLister.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+
+#include <Blast/Project/Component.hpp>
+#include <string>
+#include <vector>
+
+
+namespace Blast
+{
+   namespace Project
+   {
+      class ComponentRelativeLister
+      {
+      private:
+         Blast::Project::Component* component;
+
+      public:
+         ComponentRelativeLister(Blast::Project::Component* component=nullptr);
+         ~ComponentRelativeLister();
+
+
+      std::vector<std::string> list_component_relative_names();
+      };
+   }
+}
+
+
+
