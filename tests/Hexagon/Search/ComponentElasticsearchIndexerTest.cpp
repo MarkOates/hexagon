9f36a2993368bf1268c28839e96d3d6544c5367b
Mark Oates <markoates0@gmail.com>
2020-03-27 20:01:02 -0400

    Fix line-too-long burns

--- tests/Hexagon/Search/ComponentElasticsearchIndexerTest.cpp
+++ tests/Hexagon/Search/ComponentElasticsearchIndexerTest.cpp
@@ -20,17 +20,20 @@ TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, can_be_created_without_bl
    Hexagon::Search::ComponentElasticsearchIndexer component_elasticsearch_indexer;
 }
 
-TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, index_name__has_a_getter_and_is_set_to_the_expected_value_by_default)
+TEST(Hexagon_Search_ComponentElasticsearchIndexerTest,
+   index_name__has_a_getter_and_is_set_to_the_expected_value_by_default)
 {
    Hexagon::Search::ComponentElasticsearchIndexer indexer;
    std::string expected_default_index_name = "components_dummy";
    ASSERT_EQ(expected_default_index_name, indexer.get_index_name());
 }
 
-TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, generate_uid__with_a_nullptr_component_throws_an_error)
+TEST(Hexagon_Search_ComponentElasticsearchIndexerTest,
+   generate_uid__with_a_nullptr_component_throws_an_error)
 {
    Hexagon::Search::ComponentElasticsearchIndexer indexer(nullptr, TEST_INDEX_NAME);
-   std::string expected_error_message = "[ComponentElasticsearchIndex error:] can not \"generate_uid\" on a nullptr component";
+   std::string expected_error_message = "[ComponentElasticsearchIndex error:] can not \"generate_uid\" " \
+                                        "on a nullptr component";
    ASSERT_THROW_WITH_MESSAGE(indexer.generate_uid(), std::runtime_error, expected_error_message);
 }
 
@@ -44,14 +47,19 @@ TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, generate_uid__returns_a_u
    ASSERT_EQ(expected_uid, actual_uid);
 }
 
-TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, generate_index_shell_command__with_a_nullptr_component__throws_an_error)
+TEST(Hexagon_Search_ComponentElasticsearchIndexerTest,
+   generate_index_shell_command__with_a_nullptr_component__throws_an_error)
 {
    Hexagon::Search::ComponentElasticsearchIndexer indexer(nullptr, TEST_INDEX_NAME);
-   std::string expected_error_message = "[ComponentElasticsearchIndex error:] can not \"generate_index_shell_command\" on a nullptr component";
-   ASSERT_THROW_WITH_MESSAGE(indexer.generate_index_shell_command(), std::runtime_error, expected_error_message);
+   std::string expected_error_message = "[ComponentElasticsearchIndex error:] can not "
+                                        "\"generate_index_shell_command\" on a nullptr component";
+   ASSERT_THROW_WITH_MESSAGE(indexer.generate_index_shell_command(),
+                             std::runtime_error,
+                             expected_error_message);
 }
 
-TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, generate_index_shell_command__returns_the_expected_shell_command)
+TEST(Hexagon_Search_ComponentElasticsearchIndexerTest,
+   generate_index_shell_command__returns_the_expected_shell_command)
 {
    Blast::Project::Component component("Component/Name", "hexagon");
    Hexagon::Search::ComponentElasticsearchIndexer indexer(&component, TEST_INDEX_NAME);
@@ -64,14 +72,17 @@ TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, generate_index_shell_comm
    ASSERT_EQ(expected_shell_command, indexer.generate_index_shell_command());
 }
 
-TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, import_or_update__with_a_nullptr_component__throws_an_error)
+TEST(Hexagon_Search_ComponentElasticsearchIndexerTest,
+   import_or_update__with_a_nullptr_component__throws_an_error)
 {
    Hexagon::Search::ComponentElasticsearchIndexer indexer;
-   std::string expected_error_message = "[ComponentElasticsearchIndex error:] can not \"import_or_update\" on a nullptr component";
+   std::string expected_error_message = "[ComponentElasticsearchIndex error:] can not " \
+                                        "\"import_or_update\" on a nullptr component";
    ASSERT_THROW_WITH_MESSAGE(indexer.import_or_update(), std::runtime_error, expected_error_message);
 }
 
-TEST(Hexagon_Search_ComponentElasticsearchIndexerTest, import_or_update__imports_the_component_into_elasticsearch)
+TEST(Hexagon_Search_ComponentElasticsearchIndexerTest,
+   import_or_update__imports_the_component_into_elasticsearch)
 {
    Blast::Project::Component component("Component/Name", "hexagon");
    Hexagon::Search::ComponentElasticsearchIndexer indexer(&component, TEST_INDEX_NAME);
