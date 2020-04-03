64f617eabe55bc6e00d964b10fba0759e1f1be8b
Mark Oates <markoates0@gmail.com>
2020-04-03 17:23:23 -0400

    Add Blast/String/Trimmer

---
+++ src/Blast/String/Trimmer.cpp
@@ -0,0 +1,49 @@
+
+
+#include <Blast/String/Trimmer.hpp>
+#include <algorithm>
+
+
+namespace Blast
+{
+namespace String
+{
+
+
+Trimmer::Trimmer(std::string text)
+   : text(text)
+{
+}
+
+
+Trimmer::~Trimmer()
+{
+}
+
+
+std::string Trimmer::trim()
+{
+std::string s1 = text;
+std::string s2 = rtrim(s1);
+std::string s3 = ltrim(s2);
+return s3;
+
+}
+
+std::string Trimmer::ltrim(std::string s)
+{
+s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);}));
+return s;
+
+}
+
+std::string Trimmer::rtrim(std::string s)
+{
+s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) {return !std::isspace(c);}).base(), s.end());
+return s;
+
+}
+} // namespace String
+} // namespace Blast
+
+
---
+++ tests/Blast/String/TrimmerTest.cpp
@@ -0,0 +1,23 @@
+
+#include <gtest/gtest.h>
+
+#include <Blast/String/Trimmer.hpp>
+
+using Blast::String::Trimmer;
+
+TEST(Blast_String_TrimmerTest, can_be_created_without_arguments)
+{
+   Trimmer trimmer;
+}
+
+
+TEST(Blast_String_TrimmerTest, trim__returns_the_string_with_leading_and_trailing_whitespace_removed)
+{
+   std::string untrimmed_string = " \n  \t  This is the string  \t\n";
+   Trimmer trimmer(untrimmed_string);
+
+   std::string expected_trimmed_string = "This is the string";
+   EXPECT_EQ(expected_trimmed_string, trimmer.trim());
+}
+
+
