aea556ac79232afbbee302ca2bcc475d25345204
Mark Oates <markoates0@gmail.com>
2020-04-03 18:09:43 -0400

    Add basic initial implementation of Git/RemoteSyncChecker

--- include/Hexagon/Git/RemoteSyncChecker.hpp
+++ include/Hexagon/Git/RemoteSyncChecker.hpp
@@ -11,13 +11,17 @@ namespace Hexagon
       class RemoteSyncChecker
       {
       private:
+         std::string repo_name;
+         std::string repos_directory;
 
       public:
-         RemoteSyncChecker();
+         RemoteSyncChecker(std::string repo_name="blast", std::string repos_directory="~/Repos");
          ~RemoteSyncChecker();
 
 
-      std::string run();
+         std::string get_repo_name();
+         std::string get_repos_directory();
+      bool is_in_sync_with_remote();
       };
    }
 }
--- src/Hexagon/Git/RemoteSyncChecker.cpp
+++ src/Hexagon/Git/RemoteSyncChecker.cpp
@@ -1,7 +1,7 @@
 
 
 #include <Hexagon/Git/RemoteSyncChecker.hpp>
-
+#include <NcursesArt/GithubRepoStatusFetcher.hpp>
 
 
 namespace Hexagon
@@ -10,7 +10,9 @@ namespace Git
 {
 
 
-RemoteSyncChecker::RemoteSyncChecker()
+RemoteSyncChecker::RemoteSyncChecker(std::string repo_name, std::string repos_directory)
+   : repo_name(repo_name)
+   , repos_directory(repos_directory)
 {
 }
 
@@ -20,9 +22,23 @@ RemoteSyncChecker::~RemoteSyncChecker()
 }
 
 
-std::string RemoteSyncChecker::run()
+std::string RemoteSyncChecker::get_repo_name()
 {
-return "Hello World!";
+   return repo_name;
+}
+
+
+std::string RemoteSyncChecker::get_repos_directory()
+{
+   return repos_directory;
+}
+
+
+bool RemoteSyncChecker::is_in_sync_with_remote()
+{
+NcursesArt::GithubRepoStatusFetcher fetcher;
+return false;
+
 }
 } // namespace Git
 } // namespace Hexagon
