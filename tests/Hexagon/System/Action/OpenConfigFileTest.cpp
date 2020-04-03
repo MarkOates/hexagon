6a23f46eb751bc61fe5030e4969d9ed9a92fce1a
Mark Oates <markoates0@gmail.com>
2020-04-03 19:14:33 -0400

    Remove test in Action/OpenConfigFile

--- tests/Hexagon/System/Action/OpenConfigFileTest.cpp
+++ tests/Hexagon/System/Action/OpenConfigFileTest.cpp
@@ -8,9 +8,3 @@ TEST(Hexagon_System_Action_OpenConfigFileTest, can_be_created_without_blowing_up
    Hexagon::System::Action::OpenConfigFile open_config_file;
 }
 
-TEST(Hexagon_System_Action_OpenConfigFileTest, run__returns_the_expected_response)
-{
-   Hexagon::System::Action::OpenConfigFile open_config_file;
-   std::string expected_string = "Hello World!";
-   EXPECT_EQ(expected_string, open_config_file.run());
-}
