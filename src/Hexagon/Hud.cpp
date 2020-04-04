43e1064ad826fd1920a03c50dfc3985dd034da14
Mark Oates <markoates0@gmail.com>
2020-04-03 21:39:12 -0400

    Add some basic test for Hexagon/Hud reinitialize

--- src/Hexagon/Hud.cpp
+++ src/Hexagon/Hud.cpp
@@ -139,17 +139,21 @@ return;
 
 void Hud::reinitialize()
 {
-if (!initialized) return;
+if (!initialized)
+{
+   throw std::runtime_error("[Hud::reinitialize()] Cannot reinitialize until initialize has been called.");
+}
 if (!display)
 {
-   throw std::runtime_error("[Hud::reinitialize()] Cannot reinitialize Hud with a nullptr screen_sub_bitmap");
+   throw std::runtime_error("[Hud::reinitialize()] Cannot reinitialize Hud with a nullptr display");
 }
 if (!screen_sub_bitmap)
 {
-   throw std::runtime_error("Should not have gotten here; Expecting screen_sub_bitmap");
+   throw std::runtime_error("[Hud::reinitialize()] Should not have gotten here; Expecting screen_sub_bitmap");
 }
 
 ALLEGRO_BITMAP *backbuffer = al_get_backbuffer(display);
+al_destroy_bitmap(screen_sub_bitmap);
 ALLEGRO_BITMAP *hud_screen_sub_bitmap = al_create_sub_bitmap(backbuffer, 0, 0, al_get_bitmap_width(backbuffer), al_get_bitmap_height(backbuffer));
 set_screen_sub_bitmap(hud_screen_sub_bitmap);
 
--- tests/Hexagon/HudTest.cpp
+++ tests/Hexagon/HudTest.cpp
@@ -1,6 +1,11 @@
 
 #include <gtest/gtest.h>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
 #include <Hexagon/Hud.hpp>
 
 TEST(Hexagon_HudTest, can_be_created_without_blowing_up)
@@ -12,5 +17,13 @@ TEST(Hexagon_HudTest, notifications2__has_a_getter_and_setter)
 {
    Hexagon::Hud hud;
    ASSERT_EQ(true, hud.get_notifications2().empty());
+}
+
+TEST(Hexagon_HudTest, reinitialize__before_calling_initialize__raises_an_error)
+{
+   Hexagon::Hud hud;
+   std::string expected_message = "[Hud::reinitialize()] Cannot reinitialize until initialize has been called.";
+
+   ASSERT_THROW_WITH_MESSAGE(hud.reinitialize(), std::runtime_error, expected_message);
 }
 
