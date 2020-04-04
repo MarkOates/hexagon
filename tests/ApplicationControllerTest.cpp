a8a7b893461cab2e03f4f12194c1ff95903f9792
Mark Oates <markoates0@gmail.com>
2020-04-03 22:32:58 -0400

    Remove unused test in ApplicationController

--- tests/ApplicationControllerTest.cpp
+++ tests/ApplicationControllerTest.cpp
@@ -8,10 +8,3 @@ TEST(ApplicationControllerTest, can_be_created_without_blowing_up)
    ApplicationController application_controller;
 }
 
-TEST(ApplicationControllerTest, run__returns_the_expected_response)
-{
-   ApplicationController application_controller;
-   //std::string expected_string = "Hello World!";
-   //EXPECT_EQ(expected_string, application_controller.run());
-}
-
