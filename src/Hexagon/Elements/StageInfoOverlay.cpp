5a7b1c56df19a1b5e081f741b7c889defca34c08
Mark Oates <markoates0@gmail.com>
2020-04-07 22:57:49 -0400

    Draw full-frame on Elements/StageInfoOverlay

--- src/Hexagon/Elements/StageInfoOverlay.cpp
+++ src/Hexagon/Elements/StageInfoOverlay.cpp
@@ -1,6 +1,7 @@
 
 
 #include <Hexagon/Elements/StageInfoOverlay.hpp>
+#include <Hexagon/Elements/Frame.hpp>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_color.h>
 #include <allegro5/allegro.h>
@@ -60,9 +61,15 @@ void StageInfoOverlay::render()
 if (!title_font) throw std::runtime_error("[StageInfoOverlay::render() error]: title_font can not be a nullptr");
 if (!place) throw std::runtime_error("[StageInfoOverlay::render() error]: place can not be a nullptr");
 
+ALLEGRO_COLOR primary_color = al_color_name("orange");
+
+Hexagon::Elements::Frame frame(place->size.x, place->size.y);
+frame.set_outline_color(primary_color);
+frame.render();
+
 float line_height = al_get_font_line_height(title_font);
 //al_draw_filled_rectangle(0, 0, place->size.x, line_height*2, al_color_name("midnightblue"));
-ALLEGRO_COLOR color = al_color_name("orange");
+ALLEGRO_COLOR color = primary_color;
 //al_draw_text(title_font, color, place->size.x/2, line_height/2, ALLEGRO_ALIGN_CENTER, text.c_str());
 al_draw_text(title_font, color, place->size.x/2, place->size.y/2, ALLEGRO_ALIGN_CENTER, text.c_str());
 return;
