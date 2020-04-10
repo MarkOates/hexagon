baa5323f8996c9bce41cb5d0a2feb4827bcdaf4d
Mark Oates <markoates0@gmail.com>
2020-04-10 13:55:19 -0400

    Use intermediate variable

--- src/Hexagon/System/System.cpp
+++ src/Hexagon/System/System.cpp
@@ -591,7 +591,7 @@ bool System::save_current_stage()
    CodeEditor::Stage *stage = get_frontmost_code_editor_stage();
    if (!stage) throw std::runtime_error("Cannot save_current_stage; current stage is not a stage stage");
 
-   get_frontmost_code_editor_stage()->save_file();
+   stage->save_file();
    process_local_event(REMOVE_FILE_IS_UNSAVED_NOTIFICATION);
 
    return true;
