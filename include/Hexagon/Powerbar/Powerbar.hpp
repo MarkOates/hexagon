83d862f905b93fa43f3fabdc4954545340f920be
Mark Oates <markoates0@gmail.com>
2020-04-12 13:35:38 -0400

    Add setters for properties on Powerbar

--- include/Hexagon/Powerbar/Powerbar.hpp
+++ include/Hexagon/Powerbar/Powerbar.hpp
@@ -18,6 +18,9 @@ namespace Hexagon
          Powerbar(bool files_are_modified=true, bool files_are_committed=true, bool commits_are_in_sync_with_remote=true);
          ~Powerbar();
 
+         void set_files_are_modified(bool files_are_modified);
+         void set_files_are_committed(bool files_are_committed);
+         void set_commits_are_in_sync_with_remote(bool commits_are_in_sync_with_remote);
 
          bool get_files_are_modified();
          bool get_files_are_committed();
--- src/Hexagon/Powerbar/Powerbar.cpp
+++ src/Hexagon/Powerbar/Powerbar.cpp
@@ -23,6 +23,24 @@ Powerbar::~Powerbar()
 }
 
 
+void Powerbar::set_files_are_modified(bool files_are_modified)
+{
+   this->files_are_modified = files_are_modified;
+}
+
+
+void Powerbar::set_files_are_committed(bool files_are_committed)
+{
+   this->files_are_committed = files_are_committed;
+}
+
+
+void Powerbar::set_commits_are_in_sync_with_remote(bool commits_are_in_sync_with_remote)
+{
+   this->commits_are_in_sync_with_remote = commits_are_in_sync_with_remote;
+}
+
+
 bool Powerbar::get_files_are_modified()
 {
    return files_are_modified;
