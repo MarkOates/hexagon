b88dc21b58fadf18b45df6ac00184fc31c999d01
Mark Oates <markoates0@gmail.com>
2020-04-15 00:17:53 -0400

    Disable jumping to first search on regex modal submit

--- src/Hexagon/System/System.cpp
+++ src/Hexagon/System/System.cpp
@@ -1246,8 +1246,8 @@ bool System::submit_current_modal()
       process_local_event(DESTROY_TOPMOST_STAGE);
       process_local_event(SET_SEARCH_REGEX_EXPRESSION_ON_ALL_CODE_EDITOR_STAGES_TO_REGEX_TEMP_FILE_CONTENTS);
       process_local_event(REFRESH_REGEX_HILIGHTS_ON_ALL_CODE_EDITOR_STAGES);
-      process_local_event(JUMP_TO_NEXT_OR_NEAREST_CODE_POINT_ON_STAGE);
-      process_local_event(OFFSET_FIRST_LINE_TO_VERTICALLY_CENTER_CURSOR_ON_STAGE);
+      //process_local_event(JUMP_TO_NEXT_OR_NEAREST_CODE_POINT_ON_STAGE);
+      //process_local_event(OFFSET_FIRST_LINE_TO_VERTICALLY_CENTER_CURSOR_ON_STAGE);
       break;
    case StageInterface::GIT_COMMIT_MESSAGE_INPUT_BOX:
       process_local_event(SAVE_CURRENT_STAGE);
