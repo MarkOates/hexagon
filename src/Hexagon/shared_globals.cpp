ba9cf8e01481a97bb836a9917f72d7b9ad1fca98
Mark Oates <markoates0@gmail.com>
2020-04-19 21:25:17 -0400

    * Enable advanced line renderer

--- src/Hexagon/shared_globals.cpp
+++ src/Hexagon/shared_globals.cpp
@@ -62,7 +62,8 @@ int get_last_compiler_run_exit_code()
 
 ALLEGRO_COLOR hexagon_get_backfill_color()
 {
-   return al_color_html("5b5c60");
+   //return al_color_html("5b5c60");
+   return al_color_html("black");
 }
 
 
