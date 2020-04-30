d50c9abac40e593e35a04847a202407027422e19
Mark Oates <markoates0@gmail.com>
2020-04-29 22:17:53 -0400

    Rename Daemus/NetworkMessage -> Daemus/NetworkMessageEventController

---
+++ include/Blast/Daemus/NetworkMessageEventController.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+
+#include <Blast/Daemus/Daemus.hpp>
+#include <string>
+
+
+namespace Blast
+{
+   namespace Daemus
+   {
+      class NetworkMessageEventController
+      {
+      private:
+         Blast::Daemus::Daemus* daemus;
+
+      public:
+         NetworkMessageEventController();
+         ~NetworkMessageEventController();
+
+
+      bool process_message(std::string message="");
+      };
+   }
+}
+
+
+
---
+++ tests/Blast/Daemus/NetworkMessageEventControllerTest.cpp
@@ -0,0 +1,10 @@
+
+#include <gtest/gtest.h>
+
+#include <Blast/Daemus/NetworkMessageEventController.hpp>
+
+TEST(Blast_Daemus_NetworkMessageEventControllerTest, can_be_created_without_blowing_up)
+{
+   Blast::Daemus::NetworkMessageEventController network_message;
+}
+
