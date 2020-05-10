c170203b5bb9630093a1933bc97ae2c1626f632c
Mark Oates <markoates0@gmail.com>
2020-05-10 14:27:20 -0400

    Symlink Blast/TemplatedFile

---
+++ include/Blast/TemplatedFile.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+
+#include <string>
+#include <vector>
+
+
+namespace Blast
+{
+   class TemplatedFile
+   {
+   private:
+      std::string template_content;
+      std::vector<std::pair<std::string, std::string>> insertion_variables;
+
+   public:
+      TemplatedFile(std::string template_content="[[GREETING]], [[SUBJECT]]!", std::vector<std::pair<std::string, std::string>> insertion_variables={ { "[[GREETING]]", "Hello" }, { "[[SUBJECT]]", "World" } });
+      ~TemplatedFile();
+
+      void set_insertion_variables(std::vector<std::pair<std::string, std::string>> insertion_variables);
+
+      std::vector<std::pair<std::string, std::string>> get_insertion_variables();
+   std::string __replace(std::string str="source string", std::string from="source", std::string to="result");
+   std::string generate_content();
+   };
+}
+
+
+
---
+++ src/Blast/TemplatedFile.cpp
@@ -0,0 +1,60 @@
+
+
+#include <Blast/TemplatedFile.hpp>
+
+
+
+namespace Blast
+{
+
+
+TemplatedFile::TemplatedFile(std::string template_content, std::vector<std::pair<std::string, std::string>> insertion_variables)
+   : template_content(template_content)
+   , insertion_variables(insertion_variables)
+{
+}
+
+
+TemplatedFile::~TemplatedFile()
+{
+}
+
+
+void TemplatedFile::set_insertion_variables(std::vector<std::pair<std::string, std::string>> insertion_variables)
+{
+   this->insertion_variables = insertion_variables;
+}
+
+
+std::vector<std::pair<std::string, std::string>> TemplatedFile::get_insertion_variables()
+{
+   return insertion_variables;
+}
+
+
+std::string TemplatedFile::__replace(std::string str, std::string from, std::string to)
+{
+size_t start_pos = 0;
+while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+   str.replace(start_pos, from.length(), to);
+   start_pos += to.length();
+}
+return str;
+
+}
+
+std::string TemplatedFile::generate_content()
+{
+std::string result = template_content;
+
+for (auto &insertion_variable : insertion_variables)
+{
+  result = __replace(result, insertion_variable.first, insertion_variable.second);
+}
+
+return result;
+
+}
+} // namespace Blast
+
+
