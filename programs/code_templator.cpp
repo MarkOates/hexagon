1abe93b3d2cc3013fe896b96080470e764ec2c01
Mark Oates <markoates0@gmail.com>
2020-05-10 14:42:23 -0400

    Add divider for easier-to-read terminal output

--- programs/code_templator.cpp
+++ programs/code_templator.cpp
@@ -27,13 +27,22 @@ std::string guard_throw_with_error_message(std::string condition, std::string cl
 
 
 
+std::string divider()
+{
+   return "==========================================================";
+}
+
+
+
 #include <iostream>
 
 int main(int argc, char **argv)
 {
    std::string class_name = argv[1];
 
+   std::cout << divider() << std::endl;
    std::cout << guard_throw_with_error_message("!argument", "Foobar::ClassName", "doing_the_thing", "argument cannot be a nullptr") << std::endl;
+   std::cout << divider() << std::endl;
 
    return 0;
 }
