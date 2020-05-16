12e25fe61c56f29020fdbb2028a50e1bba95bfbf
Mark Oates <markoates0@gmail.com>
2020-05-16 14:47:39 -0400

    Add scaffolding for ComponentNavigator/SearchBar

---
+++ include/Hexagon/ComponentNavigator/SearchBar.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace ComponentNavigator
+   {
+      class SearchBar
+      {
+      private:
+
+      public:
+         SearchBar();
+         ~SearchBar();
+
+
+      std::string run();
+      };
+   }
+}
+
+
+
---
+++ src/Hexagon/ComponentNavigator/SearchBar.cpp
@@ -0,0 +1,30 @@
+
+
+#include <Hexagon/ComponentNavigator/SearchBar.hpp>
+
+
+
+namespace Hexagon
+{
+namespace ComponentNavigator
+{
+
+
+SearchBar::SearchBar()
+{
+}
+
+
+SearchBar::~SearchBar()
+{
+}
+
+
+std::string SearchBar::run()
+{
+return "Hello World!";
+}
+} // namespace ComponentNavigator
+} // namespace Hexagon
+
+
