a46eb917fd673cc7f16db709853dd246c2b0602d
Mark Oates <markoates0@gmail.com>
2020-05-17 03:56:11 -0400

    * Replace hard-coded local events if statement with map of functions

--- src/Hexagon/AdvancedComponentNavigator/EventController.cpp
+++ src/Hexagon/AdvancedComponentNavigator/EventController.cpp
@@ -1,6 +1,9 @@
 
 
 #include <Hexagon/AdvancedComponentNavigator/EventController.hpp>
+#include <map>
+#include <string>
+#include <functional>
 #include <Hexagon/AdvancedComponentNavigator/EventController.hpp>
 #include <stdexcept>
 #include <sstream>
@@ -31,9 +34,33 @@ if (!(stage))
       error_message << "EventController" << "::" << "process_local_event" << ": error: " << "guard \"stage\" not met";
       throw std::runtime_error(error_message.str());
    }
-Hexagon::AdvancedComponentNavigator::Stage &stage = *this->stage;
+using Hexagon::AdvancedComponentNavigator::Stage;
+Stage &stage = *this->stage;
 bool executed = false;
 
+std::map<std::string, std::function<void(Stage&)>> local_events = {
+   { "refresh_list", &Stage::refresh_list },
+   { "move_cursor_to_top", &Stage::move_cursor_to_top },
+   { "move_cursor_up", &Stage::move_cursor_up },
+   { "move_cursor_down", &Stage::move_cursor_down },
+   { "set_mode_to_navigating_list", &Stage::set_mode_to_navigating_list },
+   { "set_mode_to_typing_in_search_bar", &Stage::set_mode_to_typing_in_search_bar },
+};
+
+std::map<std::string, std::function<void(Stage&)>>::iterator it = local_events.find(event_name);
+if (it == local_events.end())
+{
+   std::stringstream error_message;
+   error_message << "AdvancedComponentNavigator::EventController::process_local_event: error: "
+                 << "No local event named \"" << event_name << "\" exists.";
+   throw std::runtime_error(error_message.str());
+}
+else
+{
+   local_events[event_name](stage);
+}
+
+/*
 if (event_name == "refresh_list")
 {
   executed = true;
@@ -64,8 +91,9 @@ else if (event_name == "set_mode_to_typing_in_search_bar")
   executed = true;
   stage.set_mode_to_typing_in_search_bar();
 }
+*/
 
-if (!executed)
+if (false) //(!executed)
 {
    std::stringstream error_message;
    error_message << "AdvancedComponentNavigator::EventController::process_local_event: error: "
