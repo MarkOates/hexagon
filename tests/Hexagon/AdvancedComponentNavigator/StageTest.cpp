b309bfaa125bfc89ba1f239de27ad8e9c3c8e721
Mark Oates <markoates0@gmail.com>
2020-05-17 00:31:20 -0400

    * Change order of tests in Renderer

--- tests/Hexagon/AdvancedComponentNavigator/StageTest.cpp
+++ tests/Hexagon/AdvancedComponentNavigator/StageTest.cpp
@@ -3,17 +3,6 @@
 
 #include <Hexagon/AdvancedComponentNavigator/Stage.hpp>
 
-TEST(Hexagon_AdvancedComponentNavigator_StageTest, run__returns_the_expected_response)
-{
-   Hexagon::AdvancedComponentNavigator::Stage stage;
-   std::string expected_string = "Hello World!";
-
-   stage.process_local_event("move_cursor_down");
-   stage.process_local_event("move_cursor_down");
-   stage.process_local_event("move_cursor_up");
-   stage.process_local_event("move_cursor_up");
-}
-
 TEST(Hexagon_AdvancedComponentNavigator_StageTest, type__returns_the_expected_type)
 {
    Hexagon::AdvancedComponentNavigator::Stage stage;
@@ -28,5 +17,16 @@ TEST(Hexagon_AdvancedComponentNavigator_StageTest, filter_list_with_search_strin
    StageInterface::type_t expected_type = StageInterface::COMPONENT_NAVIGATOR;
 
    ASSERT_EQ(expected_type, stage.get_type());
+}
+
+TEST(Hexagon_AdvancedComponentNavigator_StageTest, run__returns_the_expected_response)
+{
+   Hexagon::AdvancedComponentNavigator::Stage stage;
+   std::string expected_string = "Hello World!";
+
+   stage.process_local_event("move_cursor_down");
+   stage.process_local_event("move_cursor_down");
+   stage.process_local_event("move_cursor_up");
+   stage.process_local_event("move_cursor_up");
 }
 
