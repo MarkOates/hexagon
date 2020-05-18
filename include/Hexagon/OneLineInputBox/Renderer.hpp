cc477e49d3ea1e47ea04af7160ea7987d7cf1298
Mark Oates <markoates0@gmail.com>
2020-05-18 17:38:02 -0400

    * Add #render_raw# function to OneLineInputBox/Renderer

--- include/Hexagon/OneLineInputBox/Renderer.hpp
+++ include/Hexagon/OneLineInputBox/Renderer.hpp
@@ -14,6 +14,8 @@ namespace Hexagon
       private:
          ALLEGRO_FONT* font;
          ALLEGRO_COLOR outline_and_text_color;
+         float width;
+         float height;
          int cell_width;
          int cell_height;
 
@@ -22,7 +24,7 @@ namespace Hexagon
          ~Renderer();
 
 
-      void render();
+      void render_raw(int cursor_x=0, int cursor_y=0);
       };
    }
 }
--- src/Hexagon/OneLineInputBox/Renderer.cpp
+++ src/Hexagon/OneLineInputBox/Renderer.cpp
@@ -13,6 +13,8 @@ namespace OneLineInputBox
 Renderer::Renderer()
    : font(nullptr)
    , outline_and_text_color({0.0f,0.0f,0.0f,0.0f})
+   , width(1.0f)
+   , height(1.0f)
    , cell_width(1)
    , cell_height(1)
 {
@@ -24,7 +26,7 @@ Renderer::~Renderer()
 }
 
 
-void Renderer::render()
+void Renderer::render_raw(int cursor_x, int cursor_y)
 {
 return;
 
