15bc0d9a3bc04ce599f126a4deb5b68330ae9f77
Mark Oates <markoates0@gmail.com>
2020-06-14 12:43:46 -0400

    * Add Hexagon/System/EventController scaffold

---
+++ include/Hexagon/System/EventController.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace System
+   {
+      class EventController
+      {
+      private:
+
+      public:
+         EventController();
+         ~EventController();
+
+
+      std::string run();
+      };
+   }
+}
+
+
+
---
+++ src/Hexagon/System/EventController.cpp
@@ -0,0 +1,30 @@
+
+
+#include <Hexagon/System/EventController.hpp>
+
+
+
+namespace Hexagon
+{
+namespace System
+{
+
+
+EventController::EventController()
+{
+}
+
+
+EventController::~EventController()
+{
+}
+
+
+std::string EventController::run()
+{
+return "Hello World!";
+}
+} // namespace System
+} // namespace Hexagon
+
+
