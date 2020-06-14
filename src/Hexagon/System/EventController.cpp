946340fd92e658c0de58d5e71e0ffc976bce4ca3
Mark Oates <markoates0@gmail.com>
2020-06-14 13:58:59 -0400

    * Add guard on System/EventController for invalid property

--- src/Hexagon/System/EventController.cpp
+++ src/Hexagon/System/EventController.cpp
@@ -1,7 +1,8 @@
 
 
 #include <Hexagon/System/EventController.hpp>
-
+#include <stdexcept>
+#include <sstream>
 
 
 namespace Hexagon
@@ -23,6 +24,12 @@ EventController::~EventController()
 
 void EventController::process_event()
 {
+if (!(system))
+   {
+      std::stringstream error_message;
+      error_message << "EventController" << "::" << "process_event" << ": error: " << "guard \"system\" not met";
+      throw std::runtime_error(error_message.str());
+   }
 return;
 
 }
--- tests/Hexagon/System/EventControllerTest.cpp
+++ tests/Hexagon/System/EventControllerTest.cpp
@@ -3,8 +3,20 @@
 
 #include <Hexagon/System/EventController.hpp>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
 TEST(Hexagon_System_EventControllerTest, can_be_created_without_blowing_up)
 {
    Hexagon::System::EventController event_controller;
+}
+
+TEST(Hexagon_System_EventControllerTest, process_event__without_a_valid_system_raises_an_error)
+{
+   Hexagon::System::EventController event_controller;
+   std::string expected_error_message = "EventController::process_event: error: guard \"system\" not met";
+   ASSERT_THROW_WITH_MESSAGE(event_controller.process_event(), std::runtime_error, expected_error_message);
 }
 
