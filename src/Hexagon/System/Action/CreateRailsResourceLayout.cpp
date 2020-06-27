142d56dccc446a14d8cc63e2be84608c9eef8647
Mark Oates <markoates0@gmail.com>
2020-06-27 01:17:53 -0400

    * Add placement for created code editors

--- src/Hexagon/System/Action/CreateRailsResourceLayout.cpp
+++ src/Hexagon/System/Action/CreateRailsResourceLayout.cpp
@@ -48,6 +48,14 @@ std::vector<StageInterface *> &CreateRailsResourceLayout::get_dummy_stages_ref()
 bool CreateRailsResourceLayout::execute()
 {
 CodeEditor::CodeEditor *model_code_editor = new CodeEditor::CodeEditor();
+
+placement3d place(0, 0, 0);
+place.size = vec3d(400, 650, 0);
+place.align = vec3d(0.5, 0.5, 0.0);
+place.rotation = vec3d(0.0, 0.0, 0.0);
+
+model_code_editor->set_place(place);
+
 stages.push_back(model_code_editor);
 return true;
 
--- tests/Hexagon/System/Action/CreateRailsResourceLayoutTest.cpp
+++ tests/Hexagon/System/Action/CreateRailsResourceLayoutTest.cpp
@@ -42,3 +42,28 @@ TEST(Hexagon_System_Action_CreateRailsResourceLayoutTest, execute___expected_lay
    EXPECT_EQ(1, stages.size());
 }
 
+
+
+TEST(Hexagon_System_Action_CreateRailsResourceLayoutTest, execute___created_stages_have_the_expected_placement)
+{
+   std::vector<StageInterface *> stages;
+   Hexagon::System::Action::CreateRailsResourceLayout create_rails_resource_layout(stages);
+
+   EXPECT_TRUE(create_rails_resource_layout.execute());
+
+   ASSERT_EQ(1, stages.size());
+
+   placement3d expected_place(0, 0, 0);
+   expected_place.size = vec3d(400, 650, 0);
+   expected_place.align = vec3d(0.5, 0.5, 0.0);
+   expected_place.rotation = vec3d(0.0, 0.0, 0.0);
+
+   placement3d actual_place = stages[0]->get_place();
+
+   EXPECT_EQ(expected_place.size, actual_place.size);
+   EXPECT_EQ(expected_place.position, actual_place.position);
+   EXPECT_EQ(expected_place.align, actual_place.align);
+   EXPECT_EQ(expected_place.rotation, actual_place.rotation);
+   EXPECT_EQ(expected_place.scale, actual_place.scale);
+}
+
