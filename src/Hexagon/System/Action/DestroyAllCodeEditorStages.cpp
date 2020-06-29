ed466364ea8335456178eefbbbab30d5ec45d183
Mark Oates <markoates0@gmail.com>
2020-06-29 00:03:50 -0400

    Use Hexagon::CodeEditor::Stage in Action/DestroyAllCodeEditorStages

--- src/Hexagon/System/Action/DestroyAllCodeEditorStages.cpp
+++ src/Hexagon/System/Action/DestroyAllCodeEditorStages.cpp
@@ -1,7 +1,7 @@
 
 
 #include <Hexagon/System/Action/DestroyAllCodeEditorStages.hpp>
-#include <Hexagon/CodeEditor/CodeEditor.hpp>
+#include <Hexagon/CodeEditor/Stage.hpp>
 
 
 namespace Hexagon
@@ -56,7 +56,7 @@ for (unsigned i=0; i<stages.size(); i++)
         || stage->get_type() == StageInterface::MISSING_FILE
       )
    {
-      CodeEditor::CodeEditor *code_editor = static_cast<CodeEditor::CodeEditor *>(stage);
+      Hexagon::CodeEditor::Stage *code_editor = static_cast<Hexagon::CodeEditor::Stage *>(stage);
       delete code_editor;
       stages.erase(stages.begin()+i);
       i--;
