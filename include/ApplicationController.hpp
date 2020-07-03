ee394cb976d62c6a5ca7b9b8fece9b29e84ddb27
Mark Oates <markoates0@gmail.com>
2020-07-03 10:10:43 -0400

    * Add verify_presence_of_temp_files_and_assign_to_global_constants method

--- include/ApplicationController.hpp
+++ include/ApplicationController.hpp
@@ -15,6 +15,7 @@ public:
 
 
 void run_program();
+void verify_presence_of_temp_files_and_assign_to_global_constants();
 void draw_hexagon_logo_and_wait_for_keypress(int display_width=1, int display_height=1);
 };
 
