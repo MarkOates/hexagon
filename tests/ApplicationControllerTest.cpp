519fc8ab7cc0496c8df6bfe68dea99843064f91a
Mark Oates <mark.oates@shopify.com>
2020-07-06 17:14:42 -0400

    Add WIP system test

--- tests/ApplicationControllerTest.cpp
+++ tests/ApplicationControllerTest.cpp
@@ -28,3 +28,44 @@ TEST(ApplicationControllerTest, run_program__is_successful_and_will_shutdown_on_
    SUCCEED();
 }
 
+TEST(ApplicationControllerTest, run_program__will_open_the_component_navigator)
+{
+   ApplicationController application_controller(TEST_CONFIG_FILENAME);
+   application_controller.initialize();
+
+   // open component navigator
+   {
+      ALLEGRO_EVENT e;
+      e.type = ALLEGRO_EVENT_KEY_CHAR;
+      e.keyboard.keycode = ALLEGRO_KEY_TAB;
+      application_controller.emit_user_event(e);
+   }
+
+   // cursor down
+   //{
+      //ALLEGRO_EVENT ev;
+      //ev.type = ALLEGRO_EVENT_KEY_CHAR;
+      //ev.keyboard.keycode = ALLEGRO_KEY_J;
+      //application_controller.emit_user_event(ev);
+   //}
+
+   //// select option
+   //{
+      //ALLEGRO_EVENT event;
+      //event.type = ALLEGRO_EVENT_KEY_CHAR;
+      //event.keyboard.keycode = ALLEGRO_KEY_ENTER;
+      //application_controller.emit_user_event(event);
+   //}
+
+   //{
+      //ALLEGRO_EVENT display_close_event;
+      //display_close_event.type = ALLEGRO_EVENT_DISPLAY_CLOSE;
+      //application_controller.emit_user_event(display_close_event);
+   //}
+
+   application_controller.run_event_loop();
+   application_controller.shutdown();
+
+   SUCCEED();
+}
+
