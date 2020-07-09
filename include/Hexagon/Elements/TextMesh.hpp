4287fb07d3b8cf516587eac9bf464d9121200a12
Mark Oates <mark.oates@shopify.com>
2020-07-09 15:29:34 -0400

    * Add private #get_cell# method to Elements/TextMesh

--- include/Hexagon/Elements/TextMesh.hpp
+++ include/Hexagon/Elements/TextMesh.hpp
@@ -37,6 +37,7 @@ namespace Hexagon
          float get_cell_height();
          ALLEGRO_BITMAP* get_bitmap();
          static std::vector<std::vector<std::tuple<char, ALLEGRO_COLOR, ALLEGRO_COLOR>>> &get_dummy_grid_ref();
+      std::tuple<char, ALLEGRO_COLOR, ALLEGRO_COLOR>* get_cell(int x=0, int y=0);
       bool resize(int num_columns=0, int num_rows=0, float cell_width=0.0f, float cell_height=0.0f);
       void render();
       float calculate_width();
--- src/Hexagon/Elements/TextMesh.cpp
+++ src/Hexagon/Elements/TextMesh.cpp
@@ -86,6 +86,14 @@ std::vector<std::vector<std::tuple<char, ALLEGRO_COLOR, ALLEGRO_COLOR>>> &TextMe
 }
 
 
+std::tuple<char, ALLEGRO_COLOR, ALLEGRO_COLOR>* TextMesh::get_cell(int x, int y)
+{
+if (y < 0 || y >= grid.size()) return nullptr;
+if (x < 0 || x >= grid[y].size()) return nullptr;
+return nullptr;
+
+}
+
 bool TextMesh::resize(int num_columns, int num_rows, float cell_width, float cell_height)
 {
 // resize the grid
