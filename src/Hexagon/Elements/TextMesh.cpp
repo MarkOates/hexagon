2b59953d98bb14e0bedbcf7561dce1a3ee3466c6
Mark Oates <mark.oates@shopify.com>
2020-07-09 14:15:12 -0400

    * Add rendering fixtures; Draw actual mesh (though empty fixtures)

--- src/Hexagon/Elements/TextMesh.cpp
+++ src/Hexagon/Elements/TextMesh.cpp
@@ -2,6 +2,8 @@
 
 #include <Hexagon/Elements/TextMesh.hpp>
 #include <allegro5/allegro.h>
+#include <stdexcept>
+#include <sstream>
 
 
 namespace Hexagon
@@ -33,8 +35,14 @@ std::vector<std::vector<std::tuple<char, ALLEGRO_COLOR, ALLEGRO_COLOR>>> &TextMe
 
 void TextMesh::render()
 {
+if (!(al_is_system_installed()))
+   {
+      std::stringstream error_message;
+      error_message << "TextMesh" << "::" << "render" << ": error: " << "guard \"al_is_system_installed()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
 ALLEGRO_BITMAP *tile_atlas_bitmap = nullptr;
-//al_draw_prim(&vertexes[0], NULL, tile_atlas_bitmap, 0, vertexes.size(), ALLEGRO_PRIM_TRIANGLE_LIST);
+al_draw_prim(&vertexes[0], NULL, tile_atlas_bitmap, 0, vertexes.size(), ALLEGRO_PRIM_TRIANGLE_LIST);
 return;
 
 }
--- tests/Hexagon/Elements/TextMeshTest.cpp
+++ tests/Hexagon/Elements/TextMeshTest.cpp
@@ -3,14 +3,50 @@
 
 #include <Hexagon/Elements/TextMesh.hpp>
 
-TEST(Hexagon_Elements_TextMeshTest, can_be_created_without_blowing_up)
+class Hexagon_Elements_TextMeshTest_WithEmptyFixture : public ::testing::Test
+{
+public:
+   Hexagon_Elements_TextMeshTest_WithEmptyFixture() {}
+};
+
+class Hexagon_Elements_TextMeshTest_WithAllegroRenderingFixture : public ::testing::Test
+{
+public:
+   ALLEGRO_DISPLAY* display;
+
+public:
+   Hexagon_Elements_TextMeshTest_WithAllegroRenderingFixture()
+      : display(nullptr)
+   {}
+
+   virtual void SetUp() override
+   {
+      ASSERT_EQ(false, al_is_system_installed());
+      ASSERT_EQ(true, al_init());
+      ASSERT_EQ(true, al_init_primitives_addon());
+      display = al_create_display(1280, 720);
+   }
+
+   virtual void TearDown() override
+   {
+      al_destroy_display(display);
+      al_uninstall_system();
+   }
+};
+
+TEST_F(Hexagon_Elements_TextMeshTest_WithEmptyFixture, can_be_created_without_blowing_up)
 {
    Hexagon::Elements::TextMesh text_mesh;
 }
 
-TEST(Hexagon_Elements_TextMeshTest, render__works_without_blowing_up)
+TEST_F(Hexagon_Elements_TextMeshTest_WithAllegroRenderingFixture, render__works_without_blowing_up)
 {
    Hexagon::Elements::TextMesh text_mesh;
    text_mesh.render();
+
+   al_flip_display();
+
+   //sleep(1);
+
    SUCCEED();
 }
