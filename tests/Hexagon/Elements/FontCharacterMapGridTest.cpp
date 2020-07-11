0d9722f45800546ba1bea25a20af063ed4416b28
Mark Oates <markoates0@gmail.com>
2020-07-10 21:51:53 -0400

    * Fix implementation of Elements/FontCharacterMapGrid create

--- tests/Hexagon/Elements/FontCharacterMapGridTest.cpp
+++ tests/Hexagon/Elements/FontCharacterMapGridTest.cpp
@@ -7,6 +7,7 @@
    catch (...) { FAIL() << "Expected " # raised_exception_type; }
 
 #include <Hexagon/Elements/FontCharacterMapGrid.hpp>
+#include <allegro5/allegro_color.h> // for al_color_name
 #include <allegro5/allegro_image.h> // for al_save_bitmap to png
 
 TEST(Hexagon_Elements_FontCharacterMapGridTest, can_be_created_without_blowing_up)
@@ -25,6 +26,8 @@ TEST(Hexagon_Elements_FontCharacterMapGridTest, create__without_a_valid_font__ra
 TEST(Hexagon_Elements_FontCharacterMapGridTest, create__with_valid_arguments__returns_a_bitmap)
 {
    al_init();
+   ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+   al_clear_to_color(al_color_name("black"));
    ALLEGRO_FONT *font = al_create_builtin_font();
 
    Hexagon::Elements::FontCharacterMapGrid font_character_map_grid(font);
@@ -33,9 +36,14 @@ TEST(Hexagon_Elements_FontCharacterMapGridTest, create__with_valid_arguments__re
    ASSERT_NE(nullptr, result);
 
    al_init_image_addon();
-   EXPECT_EQ(true, al_save_bitmap("./tmp/FontCharacterMapGridTest.png", result));
+   al_draw_bitmap(result, 0, 0, 0);
+   al_flip_display();
+   //EXPECT_EQ(true, al_save_bitmap("./tmp/FontCharacterMapGridTest.png", result));
+
+   sleep(2);
 
    al_destroy_font(font);
+   al_destroy_display(display);
    al_uninstall_system();
 }
 
