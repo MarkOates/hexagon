7c161fc1802df6d66153c874bc08896d7fbdb4bc
Mark Oates <markoates0@gmail.com>
2020-07-10 22:25:13 -0400

    * Fill out #character_uv_mapping# on create (not tested)

--- src/Hexagon/Elements/FontCharacterMapGrid.cpp
+++ src/Hexagon/Elements/FontCharacterMapGrid.cpp
@@ -51,6 +51,8 @@ if (!(font))
       error_message << "FontCharacterMapGrid" << "::" << "create" << ": error: " << "guard \"font\" not met";
       throw std::runtime_error(error_message.str());
    }
+character_uv_mapping.clear();
+
 grid_width = al_get_text_width(font, "W"); // 'W' character as an estimate for reasonable large width
 grid_height = al_get_font_line_height(font);
 int num_rows = 10;
@@ -70,8 +72,14 @@ for (int y=0; y<=num_rows; y++)
 {
    for (int x=0; x<=num_columns; x++)
    {
+      char char_number = x + y * num_rows;
       text_to_draw[0] = x + y * num_rows;
       al_draw_text(font, text_color, x * grid_width, y * grid_height, ALLEGRO_ALIGN_LEFT, text_to_draw_cstr);
+      float u1 = x * grid_width;
+      float v1 = y * grid_height;
+      float u2 = u1 + grid_width;
+      float v2 = v2 + grid_height;
+      character_uv_mapping[char_number] = {u1, v1, u2, v2};
    }
 }
 al_restore_state(&previous_state);
