1cd17abcdac0cbbbda22b8d828befe270f8dfa03
Mark Oates <markoates0@gmail.com>
2020-07-11 11:18:39 -0400

    * Fill TextMesh example with random characters

--- examples/Hexagon/Elements/TextMeshExample.cpp
+++ examples/Hexagon/Elements/TextMeshExample.cpp
@@ -2,26 +2,36 @@
 
 #include <Hexagon/Elements/TextMesh.hpp>
 
+//#include <AllegroFlare/Random.hpp>
+
+
+void random_fill(Hexagon::Elements::TextMesh &text_mesh)
+{
+   for (unsigned y=0; y<text_mesh.get_num_rows(); y++)
+   {
+      for (unsigned x=0; x<text_mesh.get_num_columns(); x++)
+      {
+         text_mesh.set_cell_background_color(x, y, al_color_name("chartreuse"));
+         text_mesh.set_cell_character(x, y, rand()%256);
+      }
+   }
+}
+
 
 int main(int argc, char **argv)
 {
+   srand(time(0));
+
    al_init();
    ALLEGRO_DISPLAY *display = al_create_display(1280, 720);
    al_clear_to_color(al_color_name("black"));
    ALLEGRO_FONT *a_valid_font = al_create_builtin_font();
 
-   Hexagon::Elements::TextMesh text_mesh(a_valid_font, 30, 20);
+   Hexagon::Elements::TextMesh text_mesh(a_valid_font, 60, 50);
 
    text_mesh.initialize();
 
-   for (unsigned y=0; y<text_mesh.get_num_rows(); y++)
-   {
-      for (unsigned x=0; x<text_mesh.get_num_columns(); x++)
-      {
-         text_mesh.set_cell_background_color(x, y, al_color_name("red"));
-         text_mesh.set_cell_character(x, y, 'a');
-      }
-   }
+   random_fill(text_mesh);
 
    text_mesh.render();
    al_flip_display();
@@ -29,6 +39,7 @@ int main(int argc, char **argv)
    sleep(1);
 
    al_destroy_font(a_valid_font);
+   al_destroy_display(display);
    al_uninstall_system();
 
    return 0;
