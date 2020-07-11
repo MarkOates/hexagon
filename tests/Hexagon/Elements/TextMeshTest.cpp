26bd55d37299fa990177464b2e131a6b90c89670
Mark Oates <markoates0@gmail.com>
2020-07-11 17:03:48 -0400

    Expand TextMeshTest with more colorful and meaningful output

--- tests/Hexagon/Elements/TextMeshTest.cpp
+++ tests/Hexagon/Elements/TextMeshTest.cpp
@@ -108,10 +108,21 @@ TEST(Hexagon_Elements_TextMeshTest, set_cell_color__will_set_the_color_of_the_ce
    {
       for (unsigned x=0; x<text_mesh.get_num_columns(); x++)
       {
-         text_mesh.set_cell_color(x, y, al_color_name("red"));
-         text_mesh.set_cell_character(x, y, '*');
+         text_mesh.set_cell_color(x, y, al_color_name("blue"));
+         text_mesh.set_cell_character(x, y, '.');
       }
    }
+
+   text_mesh.set_cell_color(5, 5, al_color_name("yellow"));
+   text_mesh.set_cell_color(6, 5, al_color_name("yellow"));
+   text_mesh.set_cell_color(7, 5, al_color_name("yellow"));
+   text_mesh.set_cell_color(8, 5, al_color_name("yellow"));
+   text_mesh.set_cell_color(9, 5, al_color_name("yellow"));
+   text_mesh.set_cell_character(5, 5, 'H');
+   text_mesh.set_cell_character(6, 5, 'e');
+   text_mesh.set_cell_character(7, 5, 'l');
+   text_mesh.set_cell_character(8, 5, 'l');
+   text_mesh.set_cell_character(9, 5, 'o');
 
    text_mesh.render();
    al_flip_display();
