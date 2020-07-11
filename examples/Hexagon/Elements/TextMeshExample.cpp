c46052453dadb3610e2a377216f60995ed910591
Mark Oates <markoates0@gmail.com>
2020-07-11 11:38:36 -0400

    * Modify TextMesh example to randomly fill colors along with characters

--- examples/Hexagon/Elements/TextMeshExample.cpp
+++ examples/Hexagon/Elements/TextMeshExample.cpp
@@ -5,13 +5,36 @@
 #include <allegro_flare/placement3d.h> // for placement3d
 
 
+static int random_int(int min, int max)
+{
+   return rand()%(max-min+1) + min;
+}
+
+
+template<class T>
+T random_element(std::vector<T> &elements)
+{
+   return elements[random_int(0, elements.size()-1)];
+}
+
+
 void random_fill(Hexagon::Elements::TextMesh &text_mesh)
 {
+   std::vector<ALLEGRO_COLOR> colors = {
+     al_color_name("red"),
+     al_color_name("chartreuce"),
+     al_color_name("dodgerblue"),
+     al_color_name("orange"),
+     al_color_name("yellow"),
+     al_color_name("mint"),
+     al_color_name("white"),
+   };
+
    for (unsigned y=0; y<text_mesh.get_num_rows(); y++)
    {
       for (unsigned x=0; x<text_mesh.get_num_columns(); x++)
       {
-         text_mesh.set_cell_background_color(x, y, al_color_name("chartreuse"));
+         text_mesh.set_cell_background_color(x, y, random_element<ALLEGRO_COLOR>(colors));
          text_mesh.set_cell_character(x, y, rand()%256);
       }
    }
@@ -23,7 +46,7 @@ int main(int argc, char **argv)
    srand(time(0));
 
    al_init();
-   ALLEGRO_DISPLAY *display = al_create_display(1280, 720);
+   ALLEGRO_DISPLAY *display = al_create_display(1280*2, 720*2);
    al_clear_to_color(al_color_name("black"));
    ALLEGRO_FONT *a_valid_font = al_create_builtin_font();
 
@@ -35,6 +58,7 @@ int main(int argc, char **argv)
 
    placement3d place(al_get_display_width(display)/2, al_get_display_height(display)/2, 0);
    place.size = vec3d(text_mesh.calculate_width(), text_mesh.calculate_height(), 0);
+   place.scale = vec3d(0.5, 0.5, 0.5);
 
    for (unsigned i=0; i<60; i++)
    {
