f83af245b574535436886417165d2c68c71b9385
Mark Oates <markoates0@gmail.com>
2020-07-12 17:57:52 -0400

    * Convert another test to use AllegroRenderingFixture

--- tests/Hexagon/AdvancedCodeEditor/StageTest.cpp
+++ tests/Hexagon/AdvancedCodeEditor/StageTest.cpp
@@ -176,17 +176,9 @@ TEST_F(Hexagon_AdvancedCodeEditor_StageTest_WithEmptyFixture, set_initial_conten
    ASSERT_THROW_WITH_MESSAGE(stage.set_initial_content(), std::runtime_error, expected_error_message);
 }
 
-TEST_F(Hexagon_AdvancedCodeEditor_StageTest_WithEmptyFixture,
+TEST_F(Hexagon_AdvancedCodeEditor_StageTest_WithAllegroRenderingFixture,
    set_initial_content__sets_the_lines_to_the_expected_content)
 {
-   al_init();
-   al_init_primitives_addon();
-   al_init_font_addon();
-   al_init_ttf_addon();
-   ALLEGRO_DISPLAY *display = al_create_display(1280 * 2, 720 * 2);
-   AllegroFlare::FontBin font_bin;
-   font_bin.set_full_path("/Users/markoates/Repos/hexagon/bin/programs/data/fonts");
-
    Hexagon::AdvancedCodeEditor::Stage stage(&font_bin, 40, 30);
    stage.initialize();
 
@@ -200,11 +192,6 @@ TEST_F(Hexagon_AdvancedCodeEditor_StageTest_WithEmptyFixture,
    EXPECT_EQ("Confront the difficult", lines[5]);
    EXPECT_EQ("by a series of small acts.", lines[8]);
    EXPECT_EQ("thus problems are no problem for her.", lines.back());
-
-   font_bin.clear();
-   al_destroy_display(display);
-   al_shutdown_ttf_addon(); // this is required otherwise subsequent al_init_ttf_addon will not work
-   al_uninstall_system();
 }
 
 TEST_F(Hexagon_AdvancedCodeEditor_StageTest_WithEmptyFixture,
