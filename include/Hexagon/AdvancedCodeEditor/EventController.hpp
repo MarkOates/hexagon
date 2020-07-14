c94a7a2a6da0e1bbd519ea516a80b88f9b2c0ec3
Mark Oates <mark.oates@shopify.com>
2020-07-14 14:59:18 -0400

    * Add scaffold for AdvancedCodeEditor/EventController

---
+++ include/Hexagon/AdvancedCodeEditor/EventController.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace AdvancedCodeEditor
+   {
+      class EventController
+      {
+      private:
+
+      public:
+         EventController();
+         ~EventController();
+
+
+      std::string run();
+      };
+   }
+}
+
+
+
---
+++ src/Hexagon/AdvancedCodeEditor/EventController.cpp
@@ -0,0 +1,30 @@
+
+
+#include <Hexagon/AdvancedCodeEditor/EventController.hpp>
+
+
+
+namespace Hexagon
+{
+namespace AdvancedCodeEditor
+{
+
+
+EventController::EventController()
+{
+}
+
+
+EventController::~EventController()
+{
+}
+
+
+std::string EventController::run()
+{
+return "Hello World!";
+}
+} // namespace AdvancedCodeEditor
+} // namespace Hexagon
+
+
