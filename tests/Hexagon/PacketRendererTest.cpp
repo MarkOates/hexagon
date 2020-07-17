55ba30d5e5942339112f340a086c4ec44c35378e
Mark Oates <markoates0@gmail.com>
2020-07-17 11:08:47 -0400

    * Add (mostly duplicated) rendering fixtures to Hexagon/PacketRenderer

--- tests/Hexagon/PacketRendererTest.cpp
+++ tests/Hexagon/PacketRendererTest.cpp
@@ -4,7 +4,66 @@
 #include <Hexagon/PacketRenderer.hpp>
 
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro_flare/placement3d.h>
+
+class Hexagon_Elements_PacketRendererTest_WithEmptyFixture : public ::testing::Test
+{
+public:
+   Hexagon_Elements_PacketRendererTest_WithEmptyFixture() {}
+};
+
+class Hexagon_Elements_PacketRendererTest_WithAllegroRenderingFixture : public ::testing::Test
+{
+public:
+   ALLEGRO_DISPLAY* display;
+   ALLEGRO_FONT* font;
+
+public:
+   Hexagon_Elements_PacketRendererTest_WithAllegroRenderingFixture()
+      : display(nullptr)
+      , font(nullptr)
+   {}
+
+   virtual void SetUp() override
+   {
+      ASSERT_EQ(false, al_is_system_installed());
+      ASSERT_EQ(true, al_init());
+      ASSERT_EQ(true, al_init_primitives_addon());
+      display = al_create_display(1280*2, 720*2);
+      font = al_create_builtin_font();
+      al_clear_to_color(ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f});
+   }
+
+   virtual void TearDown() override
+   {
+      if (font) al_destroy_font(font);
+      al_destroy_display(display);
+      al_uninstall_system();
+   }
+
+   placement3d centered_placement(float width, float height)
+   {
+      placement3d place(al_get_display_width(display)/2, al_get_display_height(display)/2, 0.0f);
+      place.size = vec3d(width, height, 0);
+      return place;
+   }
+
+   void draw_current_test_name()
+   {
+      std::string current_test_name = ::testing::UnitTest::GetInstance()->current_test_info()->name();
+      ALLEGRO_COLOR color = ALLEGRO_COLOR{1.0f, 1.0f, 1.0f, 1.0f};
+      al_draw_text(
+         font,
+         color,
+         al_get_display_width(display)/2,
+         al_get_display_height(display)/4,
+         ALLEGRO_ALIGN_CENTER,
+         current_test_name.c_str()
+      );
+   }
+};
 
 TEST(Hexagon_PacketRendererTest, can_be_created_without_blowing_up)
 {
