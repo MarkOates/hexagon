89f0bebd8155392337852527499abfc8a3023cd1
Mark Oates <markoates0@gmail.com>
2020-07-17 11:11:45 -0400

    * Modify Hexagon/PacketRenderer tests to use provided fixtures

--- tests/Hexagon/PacketRendererTest.cpp
+++ tests/Hexagon/PacketRendererTest.cpp
@@ -65,25 +65,19 @@ public:
    }
 };
 
-TEST(Hexagon_PacketRendererTest, can_be_created_without_blowing_up)
+TEST_F(Hexagon_Elements_PacketRendererTest_WithEmptyFixture,
+   can_be_created_without_blowing_up)
 {
    Hexagon::PacketRenderer packet_renderer;
 }
 
-TEST(Hexagon_PacketRendererTest, render__will_render_the_packet)
-{
-   al_init();
-   al_init_primitives_addon();
-   ALLEGRO_BITMAP *render_surface = al_create_bitmap(800, 400);
-   al_set_target_bitmap(render_surface);
 
+TEST_F(Hexagon_Elements_PacketRendererTest_WithAllegroRenderingFixture,
+   render__will_render_the_packet)
+{
    Hexagon::Packet packet;
 
    Hexagon::PacketRenderer packet_renderer(&packet);
    packet_renderer.render();
-
-   al_uninstall_system();
-
-   SUCCEED();
 }
 
