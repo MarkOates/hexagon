6eeeba28076a840bb193fb5140a923593c139566
Mark Oates <markoates0@gmail.com>
2020-07-18 10:18:39 -0400

    * Add test to check for rendering #active# and #bg# bars together

--- tests/Hexagon/Elements/SingleBlockBarGraphTest.cpp
+++ tests/Hexagon/Elements/SingleBlockBarGraphTest.cpp
@@ -133,3 +133,26 @@ TEST_F(Hexagon_Elements_SingleBlockBarGraphTest_WithAllegroRenderingFixture,
    sleep(1);
 }
 
+TEST_F(Hexagon_Elements_SingleBlockBarGraphTest_WithAllegroRenderingFixture,
+   draw__will_render_the_correct_active_and_non_active_bars)
+{
+   Hexagon::Elements::SingleBlockBarGraph single_block_bar_graph;
+   placement3d place = build_centered_placement(0, 0);
+
+   single_block_bar_graph.set_number_of_bg_bars(8);
+   single_block_bar_graph.set_number_of_active_bars(5);
+   single_block_bar_graph.set_bar_width(20);
+   single_block_bar_graph.set_bar_height(60);
+   single_block_bar_graph.set_bar_spacing(30);
+   single_block_bar_graph.set_main_bar_color(ALLEGRO_COLOR{0.0f, 0.4f, 0.4f, 1.0f});
+   single_block_bar_graph.set_bg_bar_color(ALLEGRO_COLOR{0.2f, 0.2f, 0.2f, 1.0f});
+
+   place.start_transform();
+   single_block_bar_graph.draw();
+   place.restore_transform();
+
+   al_flip_display();
+
+   sleep(1);
+}
+
