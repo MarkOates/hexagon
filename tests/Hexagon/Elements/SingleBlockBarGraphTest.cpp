b71d24011541ef73fa0285068a8f264d9c534377
Mark Oates <markoates0@gmail.com>
2020-07-18 09:51:14 -0400

    * Add rendering fixture to Elements/SingleBlockBarGraph

--- tests/Hexagon/Elements/SingleBlockBarGraphTest.cpp
+++ tests/Hexagon/Elements/SingleBlockBarGraphTest.cpp
@@ -9,11 +9,45 @@
 #include <Hexagon/Elements/SingleBlockBarGraph.hpp>
 
 #include <allegro5/allegro_primitives.h>
+#include <allegro_flare/placement3d.h>
 
 class Hexagon_Elements_SingleBlockBarGraphTest_WithEmptyFixture : public ::testing::Test
 {
 public:
    Hexagon_Elements_SingleBlockBarGraphTest_WithEmptyFixture() {}
+};
+
+class Hexagon_Elements_SingleBlockBarGraphTest_WithAllegroRenderingFixture : public ::testing::Test
+{
+public:
+   ALLEGRO_DISPLAY* display;
+
+public:
+   Hexagon_Elements_SingleBlockBarGraphTest_WithAllegroRenderingFixture()
+      : display(nullptr)
+   {}
+
+   virtual void SetUp() override
+   {
+      ASSERT_EQ(false, al_is_system_installed());
+      ASSERT_EQ(true, al_init());
+      ASSERT_EQ(true, al_init_primitives_addon());
+      display = al_create_display(1280*2, 720*2);
+      al_clear_to_color(ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f});
+   }
+
+   virtual void TearDown() override
+   {
+      al_destroy_display(display);
+      al_uninstall_system();
+   }
+
+   placement3d build_centered_placement(float width, float height)
+   {
+      placement3d place(al_get_display_width(display)/2, al_get_display_height(display)/2, 0.0f);
+      place.size = vec3d(width, height, 0);
+      return place;
+   }
 };
 
 TEST_F(Hexagon_Elements_SingleBlockBarGraphTest_WithEmptyFixture,
