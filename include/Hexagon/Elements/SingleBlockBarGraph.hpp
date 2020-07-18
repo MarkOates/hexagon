455843849650eceb5173b6252d84d0881505a7aa
Mark Oates <markoates0@gmail.com>
2020-07-18 09:21:20 -0400

    * Add properties to Elements/SingleBlockBarGraph

--- include/Hexagon/Elements/SingleBlockBarGraph.hpp
+++ include/Hexagon/Elements/SingleBlockBarGraph.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <allegro5/allegro.h>
 #include <string>
 
 
@@ -11,9 +12,18 @@ namespace Hexagon
       class SingleBlockBarGraph
       {
       private:
+         int number_of_active_bars;
+         int number_of_bg_bars;
+         float bar_width;
+         float bar_height;
+         float bar_spacing;
+         float stroke_width;
+         ALLEGRO_COLOR main_bar_color;
+         ALLEGRO_COLOR bg_bar_color;
+         ALLEGRO_COLOR stroke_color;
 
       public:
-         SingleBlockBarGraph();
+         SingleBlockBarGraph(int number_of_active_bars=0, int number_of_bg_bars=0, float bar_width=1.0f, float bar_height=1.0f, float bar_spacing=0.0f, float stroke_width=1.0f, ALLEGRO_COLOR main_bar_color=ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f}, ALLEGRO_COLOR bg_bar_color=ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f}, ALLEGRO_COLOR stroke_color=ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f});
          ~SingleBlockBarGraph();
 
 
--- src/Hexagon/Elements/SingleBlockBarGraph.cpp
+++ src/Hexagon/Elements/SingleBlockBarGraph.cpp
@@ -10,7 +10,16 @@ namespace Elements
 {
 
 
-SingleBlockBarGraph::SingleBlockBarGraph()
+SingleBlockBarGraph::SingleBlockBarGraph(int number_of_active_bars, int number_of_bg_bars, float bar_width, float bar_height, float bar_spacing, float stroke_width, ALLEGRO_COLOR main_bar_color, ALLEGRO_COLOR bg_bar_color, ALLEGRO_COLOR stroke_color)
+   : number_of_active_bars(number_of_active_bars)
+   , number_of_bg_bars(number_of_bg_bars)
+   , bar_width(bar_width)
+   , bar_height(bar_height)
+   , bar_spacing(bar_spacing)
+   , stroke_width(stroke_width)
+   , main_bar_color(main_bar_color)
+   , bg_bar_color(bg_bar_color)
+   , stroke_color(stroke_color)
 {
 }
 
