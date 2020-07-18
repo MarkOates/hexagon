7f6ac6070b6abb5d8f1f26a397b332ad41eec0ef
Mark Oates <markoates0@gmail.com>
2020-07-18 16:25:54 -0400

    * Fix method name in test description

--- tests/Hexagon/Git/UntrackedTest.cpp
+++ tests/Hexagon/Git/UntrackedTest.cpp
@@ -8,7 +8,7 @@ TEST(Hexagon_Git_UntrackedTest, can_be_created_without_blowing_up)
    Hexagon::Git::Untracked untracked;
 }
 
-TEST(Hexagon_Git_UntrackedTest, build_staged_files_shell_command__returns_the_expected_value)
+TEST(Hexagon_Git_UntrackedTest, build_tracked_files_shell_command__returns_the_expected_value)
 {
    Hexagon::Git::Untracked untracked("/A/Directory/Where/A/Project/Is/");
    std::string expected_command = "(cd /A/Directory/Where/A/Project/Is/ && git ls-files --others --exclude-standard)";
