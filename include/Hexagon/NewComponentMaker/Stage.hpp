804da41168f17e6ebd14bad332e43563fe406a45
Mark Oates <markoates0@gmail.com>
2020-07-19 16:20:57 -0400

    * Remove core feature set from NewComponentMaker/Stage

--- include/Hexagon/NewComponentMaker/Stage.hpp
+++ include/Hexagon/NewComponentMaker/Stage.hpp
@@ -5,7 +5,6 @@
 #include <Hexagon/StageInterface.hpp>
 #include <allegro5/allegro.h>
 #include <string>
-#include <vector>
 
 
 namespace Hexagon
@@ -17,9 +16,6 @@ namespace Hexagon
       private:
          std::string current_project_directory;
          std::string component_name;
-         bool shell_command_has_executed;
-         std::string last_executed_shell_response;
-         std::string path_to_executable;
          static ALLEGRO_EVENT a_default_empty_event;
 
       public:
@@ -32,9 +28,6 @@ namespace Hexagon
       virtual void render() override;
       void process_local_event(std::string event_name="", ActionData action_data=ActionData());
       void process_event(ALLEGRO_EVENT& event=get_a_default_empty_event_ref());
-      std::string get_shell_response();
-      std::vector<std::string> get_shell_response_lines();
-      std::string build_shell_command();
       };
    }
 }
--- src/Hexagon/NewComponentMaker/Stage.cpp
+++ src/Hexagon/NewComponentMaker/Stage.cpp
@@ -2,12 +2,6 @@
 
 #include <Hexagon/NewComponentMaker/Stage.hpp>
 #include <allegro5/allegro_primitives.h>
-#include <sstream>
-#include <Blast/ShellCommandExecutorWithCallback.hpp>
-#include <Blast/StringSplitter.hpp>
-#include <sstream>
-#include <Blast/ShellCommandExecutorWithCallback.hpp>
-#include <Blast/StringSplitter.hpp>
 
 
 namespace Hexagon
@@ -23,9 +17,6 @@ Stage::Stage(std::string current_project_directory, std::string component_name)
    : StageInterface(StageInterface::NEW_COMPONENT_MAKER)
    , current_project_directory(current_project_directory)
    , component_name(component_name)
-   , shell_command_has_executed(false)
-   , last_executed_shell_response("")
-   , path_to_executable("~/Repos/blast/bin/programs/component_generator")
 {
 }
 
@@ -63,37 +54,6 @@ return;
 void Stage::process_event(ALLEGRO_EVENT& event)
 {
 return;
-
-}
-
-std::string Stage::get_shell_response()
-{
-if (shell_command_has_executed) return last_executed_shell_response;
-Blast::ShellCommandExecutorWithCallback executor(
-      build_shell_command(),
-      Blast::ShellCommandExecutorWithCallback::simple_silent_callback
-   );
-last_executed_shell_response = executor.execute();
-return last_executed_shell_response;
-
-}
-
-std::vector<std::string> Stage::get_shell_response_lines()
-{
-return Blast::StringSplitter(get_shell_response(), '\n').split();
-
-}
-
-std::string Stage::build_shell_command()
-{
-std::stringstream commit_everything_shell_command;
-commit_everything_shell_command << "("
-                                << "cd " << current_project_directory
-                                << " && "
-                                << path_to_executable << " " << component_name
-                                << ")";
-
-return commit_everything_shell_command.str();
 
 }
 } // namespace NewComponentMaker
