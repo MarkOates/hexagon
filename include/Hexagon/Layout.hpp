ad75c4593b7bc8c2ebfb5b3cab4354b2ab8adc61
Mark Oates <mark.oates@shopify.com>
2020-07-20 10:56:55 -0400

    * Add a lot more properties to Hexagon/Layout

--- include/Hexagon/Layout.hpp
+++ include/Hexagon/Layout.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <Hexagon/StageInterface.hpp>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -14,16 +15,26 @@ namespace Hexagon
       std::string project_root;
       std::vector<std::tuple<std::string, float, float>> file;
       std::string daemus_command;
+      int display_default_height;
+      int code_editor_width;
+      std::vector<StageInterface *>& stages;
+      static std::vector<StageInterface *> dummy_stages;
 
    public:
-      Layout(std::string project_root="", std::vector<std::tuple<std::string, float, float>> file={}, std::string daemus_command="");
+      Layout(std::string project_root="", std::vector<std::tuple<std::string, float, float>> file={}, std::string daemus_command="", int display_default_height=1350, int code_editor_width=1215, std::vector<StageInterface *>& stages=get_dummy_stages_ref());
       ~Layout();
 
       void set_file(std::vector<std::tuple<std::string, float, float>> file);
+      void set_stages(std::vector<StageInterface *>& stages);
 
       std::string get_project_root();
       std::vector<std::tuple<std::string, float, float>> get_file();
       std::string get_daemus_command();
+      int get_display_default_height();
+      int get_code_editor_width();
+      std::vector<StageInterface *>& get_stages();
+      static std::vector<StageInterface *> &get_dummy_stages_ref();
+   bool place_and_load_code_editor(std::vector<StageInterface *>* stages_ptr=nullptr, std::string filename="", std::string file_category="undefined_file_category", float x=0.0f, float y=0.0f);
    };
 }
 
--- src/Hexagon/Layout.cpp
+++ src/Hexagon/Layout.cpp
@@ -1,17 +1,24 @@
 
 
 #include <Hexagon/Layout.hpp>
-
+#include <allegro_flare/useful_php.h>
+#include <Hexagon/CodeEditor/Stage.hpp>
 
 
 namespace Hexagon
 {
 
 
-Layout::Layout(std::string project_root, std::vector<std::tuple<std::string, float, float>> file, std::string daemus_command)
+std::vector<StageInterface *> Layout::dummy_stages = {};
+
+
+Layout::Layout(std::string project_root, std::vector<std::tuple<std::string, float, float>> file, std::string daemus_command, int display_default_height, int code_editor_width, std::vector<StageInterface *>& stages)
    : project_root(project_root)
    , file(file)
    , daemus_command(daemus_command)
+   , display_default_height(display_default_height)
+   , code_editor_width(code_editor_width)
+   , stages(stages)
 {
 }
 
@@ -24,6 +31,12 @@ Layout::~Layout()
 void Layout::set_file(std::vector<std::tuple<std::string, float, float>> file)
 {
    this->file = file;
+}
+
+
+void Layout::set_stages(std::vector<StageInterface *>& stages)
+{
+   this->stages = stages;
 }
 
 
@@ -45,6 +58,51 @@ std::string Layout::get_daemus_command()
 }
 
 
+int Layout::get_display_default_height()
+{
+   return display_default_height;
+}
+
+
+int Layout::get_code_editor_width()
+{
+   return code_editor_width;
+}
+
+
+std::vector<StageInterface *>& Layout::get_stages()
+{
+   return stages;
+}
+
+
+std::vector<StageInterface *> &Layout::get_dummy_stages_ref()
+{
+   return dummy_stages;
+}
+
+
+bool Layout::place_and_load_code_editor(std::vector<StageInterface *>* stages_ptr, std::string filename, std::string file_category, float x, float y)
+{
+float code_editor_height = get_display_default_height();
+{
+   std::string file_contents = php::file_get_contents(filename);
+
+   Hexagon::CodeEditor::Stage *stage = new Hexagon::CodeEditor::Stage({filename, file_category});
+   stage->get_code_editor_ref().set_initial_content(file_contents);
+
+   placement3d place(x * code_editor_width, y * code_editor_height, 0);
+   place.size = vec3d(code_editor_width, code_editor_height, 0);
+   place.align = vec3d(0.5, 0.5, 0.0);
+   place.rotation = vec3d(0.0, 0.0, 0.0);
+
+   stage->set_place(place);
+
+   stages.push_back(stage);
+}
+return true;
+
+}
 } // namespace Hexagon
 
 
