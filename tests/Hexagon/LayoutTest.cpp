be6aba8c92f17eef1fd89116ad6ab2d11b61b93b
Mark Oates <markoates0@gmail.com>
2020-07-20 22:13:52 -0400

    * Add test to check for created CODE_EDITOR type in Hexagon/Layout

--- tests/Hexagon/LayoutTest.cpp
+++ tests/Hexagon/LayoutTest.cpp
@@ -24,3 +24,23 @@ TEST(Hexagon_LayoutTest, create__will_create_the_passed_files)
    ASSERT_EQ(false, stages.empty());
 }
 
+TEST(Hexagon_LayoutTest, create__will_create_code_editor_stages_for_the_passed_files)
+{
+   std::string project_root = "/Users/markoates/Repos/hexagon/";
+   std::vector<StageInterface *> stages;
+   std::string daemus_command = "git diff";
+   std::vector<std::tuple<std::string, std::string, float, float>> files = {
+      { "/Users/markoates/Repos/hexagon/quintessence/Hexagon/Logo.q.yml", "blast_quintessence", 0, 0 },
+      { "/Users/markoates/Repos/hexagon/tests/Hexagon/LogoTest.cpp", "blast_test", 100, 200 },
+   };
+
+   Hexagon::Layout layout(project_root, &stages, files, daemus_command);
+
+   layout.create();
+
+   for (auto &stage : stages)
+   {
+      EXPECT_EQ(StageInterface::CODE_EDITOR, stage->get_type());
+   }
+}
+
