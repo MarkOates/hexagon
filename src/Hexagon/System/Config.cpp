8ae66f93ba781ca199dcd8842d9ea5e976b5173e
Mark Oates <mark.oates@shopify.com>
2020-07-22 13:45:14 -0400

    * Configure non-dark mode to have solid white background

--- src/Hexagon/System/Config.cpp
+++ src/Hexagon/System/Config.cpp
@@ -165,7 +165,8 @@ if (is_dark_mode())
 }
 else
 {
-   return al_color_html("8f9996"); // deep rich gray
+   return al_color_name("white");
+   //return al_color_html("8f9996"); // deep rich gray
    //return al_color_html("8a5b38"); // darker, more true deep brown from lamp
    //return al_color_html("a67d5a"); // color of lamp light against wall
 
