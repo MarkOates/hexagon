7a9e9db4bae25451d46d7707b2ece2958f7ede09
Mark Oates <mark.oates@shopify.com>
2020-07-23 10:14:18 -0400

    * Add pieces to pass to return value

--- src/Hexagon/ProjectFactory.cpp
+++ src/Hexagon/ProjectFactory.cpp
@@ -20,8 +20,11 @@ ProjectFactory::~ProjectFactory()
 
 Hexagon::Project ProjectFactory::partner_performance_dashboard()
 {
-Hexagon::Project result;
-return {};
+std::string project_name = "Partner Performance Dashboard";
+std::string project_root = "/Users/markoates/Repos/hexagon/";
+std::vector<std::tuple<std::string, Hexagon::Layout>> layouts = {};
+
+return Hexagon::Project(project_name, project_root, layouts);
 
 }
 } // namespace Hexagon
