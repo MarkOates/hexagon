5ee556dc7368a734012a795e2d057b7f36c1e92e
Mark Oates <mark.oates@shopify.com>
2020-07-23 15:45:25 -0400

    * Add more fields to rudimentary hard-coded project in ProjectFactory

--- src/Hexagon/ProjectFactory.cpp
+++ src/Hexagon/ProjectFactory.cpp
@@ -20,9 +20,31 @@ ProjectFactory::~ProjectFactory()
 
 Hexagon::Project ProjectFactory::partner_performance_dashboard()
 {
+type: std::vector<StageInterface *>* stages = nullptr;
+
 std::string project_name = "Partner Performance Dashboard";
-std::string project_root = "/Users/markoates/Repos/hexagon/";
-std::vector<std::tuple<std::string, Hexagon::Layout>> layouts = {};
+std::string project_root = "/Users/markoates/dev_repos/partners/";
+std::vector<std::tuple<std::string, Hexagon::Layout>> layouts = {
+   { "",
+      {
+         "models/ES::StoreRepository",
+         project_root,
+         stages,
+         {
+            {
+               "/Users/markoates/dev_repos/partners/app/models/es/store_repository.rb",
+               "rails_model",
+               {},
+            },
+            {
+               "/Users/markoates/dev_repos/partners/spec/models/es/store_repository_spec.rb",
+               "rails_spec",
+               {},
+            }
+         },
+      },
+   }
+};
 
 return Hexagon::Project(project_name, project_root, layouts);
 
