274009570b8f233f98f236f281718d791a500547
Mark Oates <mark.oates@shopify.com>
2020-07-23 10:08:35 -0400

    * Add scaffold for Hexagon/ProjetFactory

---
+++ include/Hexagon/ProjectFactory.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   class ProjectFactory
+   {
+   private:
+
+   public:
+      ProjectFactory();
+      ~ProjectFactory();
+
+
+   std::string run();
+   };
+}
+
+
+
---
+++ src/Hexagon/ProjectFactory.cpp
@@ -0,0 +1,27 @@
+
+
+#include <Hexagon/ProjectFactory.hpp>
+
+
+
+namespace Hexagon
+{
+
+
+ProjectFactory::ProjectFactory()
+{
+}
+
+
+ProjectFactory::~ProjectFactory()
+{
+}
+
+
+std::string ProjectFactory::run()
+{
+return "Hello World!";
+}
+} // namespace Hexagon
+
+
