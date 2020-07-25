a39eac0d6316f1d96e3457052bfaae2631c3e3ae
Mark Oates <markoates0@gmail.com>
2020-07-25 11:56:29 -0400

    * Add some basic dependencies for Forms/Fun

--- src/Hexagon/Forms/Fun.cpp
+++ src/Hexagon/Forms/Fun.cpp
@@ -1,7 +1,11 @@
 
 
 #include <Hexagon/Forms/Fun.hpp>
-
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro.h>
+#include <stdexcept>
+#include <sstream>
 
 
 namespace Hexagon
@@ -22,6 +26,24 @@ Fun::~Fun()
 
 std::string Fun::run()
 {
+if (!(al_is_system_installed()))
+   {
+      std::stringstream error_message;
+      error_message << "Fun" << "::" << "run" << ": error: " << "guard \"al_is_system_installed()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+if (!(al_is_primitives_addon_initialized()))
+   {
+      std::stringstream error_message;
+      error_message << "Fun" << "::" << "run" << ": error: " << "guard \"al_is_primitives_addon_initialized()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+if (!(al_get_target_bitmap()))
+   {
+      std::stringstream error_message;
+      error_message << "Fun" << "::" << "run" << ": error: " << "guard \"al_get_target_bitmap()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
 return "Hello World!";
 }
 } // namespace Forms
--- tests/Hexagon/Forms/FunTest.cpp
+++ tests/Hexagon/Forms/FunTest.cpp
@@ -3,6 +3,9 @@
 
 #include <Hexagon/Forms/Fun.hpp>
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
 TEST(Hexagon_Forms_FunTest, can_be_created_without_blowing_up)
 {
    Hexagon::Forms::Fun fun;
@@ -10,7 +13,14 @@ TEST(Hexagon_Forms_FunTest, can_be_created_without_blowing_up)
 
 TEST(Hexagon_Forms_FunTest, run__returns_the_expected_response)
 {
+   al_init();
+   al_init_primitives_addon();
+   ALLEGRO_BITMAP *render_surface = al_create_bitmap(16 * 80, 9 * 80);
+   al_set_target_bitmap(render_surface);
+
    Hexagon::Forms::Fun fun;
    std::string expected_string = "Hello World!";
    EXPECT_EQ(expected_string, fun.run());
+
+   al_uninstall_system();
 }
