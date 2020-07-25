1924a29329fd43b56f53844c6172c2ca4f51fc36
Mark Oates <markoates0@gmail.com>
2020-07-25 13:13:01 -0400

    * Transfer some properties of Forms/Fun over to parameters for #draw# method

--- include/Hexagon/Forms/Fun.hpp
+++ include/Hexagon/Forms/Fun.hpp
@@ -13,21 +13,15 @@ namespace Hexagon
       class Fun
       {
       private:
-         placement3d place;
-         ALLEGRO_COLOR backfill_color;
          std::string buffer;
 
       public:
-         Fun(placement3d place={}, ALLEGRO_COLOR backfill_color=ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f});
+         Fun();
          ~Fun();
 
-         void set_place(placement3d place);
-         void set_backfill_color(ALLEGRO_COLOR backfill_color);
 
-         placement3d get_place();
-         ALLEGRO_COLOR get_backfill_color();
          std::string get_buffer();
-      void draw();
+      void draw(placement3d place={}, ALLEGRO_COLOR backfill_color=ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f});
       int process_char_input(char input_ch=0);
       };
    }
--- src/Hexagon/Forms/Fun.cpp
+++ src/Hexagon/Forms/Fun.cpp
@@ -14,10 +14,8 @@ namespace Forms
 {
 
 
-Fun::Fun(placement3d place, ALLEGRO_COLOR backfill_color)
-   : place(place)
-   , backfill_color(backfill_color)
-   , buffer("")
+Fun::Fun()
+   : buffer("")
 {
 }
 
@@ -27,37 +25,13 @@ Fun::~Fun()
 }
 
 
-void Fun::set_place(placement3d place)
-{
-   this->place = place;
-}
-
-
-void Fun::set_backfill_color(ALLEGRO_COLOR backfill_color)
-{
-   this->backfill_color = backfill_color;
-}
-
-
-placement3d Fun::get_place()
-{
-   return place;
-}
-
-
-ALLEGRO_COLOR Fun::get_backfill_color()
-{
-   return backfill_color;
-}
-
-
 std::string Fun::get_buffer()
 {
    return buffer;
 }
 
 
-void Fun::draw()
+void Fun::draw(placement3d place, ALLEGRO_COLOR backfill_color)
 {
 if (!(al_is_system_installed()))
    {
