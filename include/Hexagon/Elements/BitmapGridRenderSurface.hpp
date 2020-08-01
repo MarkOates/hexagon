99bcc4c61cebb4e8f78a17ea0152aeb8031ab0df
Mark Oates <markoates0@gmail.com>
2020-08-01 01:12:38 -0400

    * Add scaffold files for Elements/BitmapGridRenderSurface

---
+++ include/Hexagon/Elements/BitmapGridRenderSurface.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace Elements
+   {
+      class BitmapGridRenderSurface
+      {
+      private:
+
+      public:
+         BitmapGridRenderSurface();
+         ~BitmapGridRenderSurface();
+
+
+      std::string run();
+      };
+   }
+}
+
+
+
---
+++ src/Hexagon/Elements/BitmapGridRenderSurface.cpp
@@ -0,0 +1,30 @@
+
+
+#include <Hexagon/Elements/BitmapGridRenderSurface.hpp>
+
+
+
+namespace Hexagon
+{
+namespace Elements
+{
+
+
+BitmapGridRenderSurface::BitmapGridRenderSurface()
+{
+}
+
+
+BitmapGridRenderSurface::~BitmapGridRenderSurface()
+{
+}
+
+
+std::string BitmapGridRenderSurface::run()
+{
+return "Hello World!";
+}
+} // namespace Elements
+} // namespace Hexagon
+
+
