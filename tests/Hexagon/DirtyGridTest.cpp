0b474918e66595ddb60a1d311e00c98941977df3
Mark Oates <markoates0@gmail.com>
2020-08-02 11:36:37 -0400

    * Add test for #mark_as_dirty#

--- tests/Hexagon/DirtyGridTest.cpp
+++ tests/Hexagon/DirtyGridTest.cpp
@@ -8,3 +8,18 @@ TEST(Hexagon_DirtyGridTest, can_be_created_without_blowing_up)
    Hexagon::DirtyGrid dirty_grid;
 }
 
+TEST(Hexagon_DirtyGridTest, mark_as_dirty__will_include_the_item_in_the_list)
+{
+   Hexagon::DirtyGrid dirty_grid;
+   std::set<std::pair<int, int>> dirty_cells;
+
+   dirty_cells = dirty_grid.get_dirty_cells();
+   ASSERT_EQ(0, dirty_cells.size());
+
+   dirty_grid.mark_as_dirty(3, 5);
+   dirty_grid.mark_as_dirty(7, 13);
+
+   dirty_cells = dirty_grid.get_dirty_cells();
+   ASSERT_EQ(2, dirty_cells.size());
+}
+
