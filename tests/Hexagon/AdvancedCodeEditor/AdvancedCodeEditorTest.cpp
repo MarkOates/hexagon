f467f6b31834a935437271a0d3e9c47f1bee901a
Mark Oates <markoates0@gmail.com>
2020-08-02 16:48:08 -0400

    * Add check and test for #below lines before# for #split_lines#

--- tests/Hexagon/AdvancedCodeEditor/AdvancedCodeEditorTest.cpp
+++ tests/Hexagon/AdvancedCodeEditor/AdvancedCodeEditorTest.cpp
@@ -325,7 +325,12 @@ and it will spill.)END";
    advanced_code_editor.cursor_move_to(14, 1);
    advanced_code_editor.split_lines();
 
-   std::vector<std::pair<int, int>> expected_cells_from_before_change = { { 0, 2 }, { 1, 2 }, { 2, 2 } };
+   std::vector<std::pair<int, int>> expected_cells_from_before_change = {
+      { 0, 2 }, { 1, 2 }, { 2, 2 },
+
+      { 0, 3 }, { 1, 3 }, { 2, 3 }, { 3, 3 }, { 4, 3 }, { 5, 3 }, { 6, 3 }, { 7, 3 }, { 8, 3 }, { 9, 3 }, { 10, 3 },
+      { 11, 3 }, { 12, 3 }, { 13, 3 }, { 14, 3 }, { 15, 3 }, { 16, 3 }, { 17, 3 },
+   };
    std::vector<std::pair<int, int>> actual = advanced_code_editor.get_dirty_cells();
    ASSERT_THAT(expected_cells_from_before_change, IsSubsetOf(actual));
 }
