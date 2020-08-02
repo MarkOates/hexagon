affa5602b37d1e89cabf1f9ce5cc94de0b87793a
Mark Oates <markoates0@gmail.com>
2020-08-02 16:56:08 -0400

    * Duplicate test data into test in prep of implemented logic

--- tests/Hexagon/AdvancedCodeEditor/AdvancedCodeEditorTest.cpp
+++ tests/Hexagon/AdvancedCodeEditor/AdvancedCodeEditorTest.cpp
@@ -345,3 +345,47 @@ and it will spill.)END";
    ASSERT_THAT(expected_cells_from_after_change, IsSubsetOf(actual));
 }
 
+TEST(Hexagon_AdvancedCodeEditor_AdvancedCodeEditorTest,
+   split_lines__will_set_the_dirty_cells_with_only_the_expected_cells)
+{
+   std::string lines = R"END(        - 9 -
+Fill your bowl to the brim
+
+and it will spill.)END";
+
+   Hexagon::AdvancedCodeEditor::AdvancedCodeEditor advanced_code_editor;
+   advanced_code_editor.set_content(lines);
+   advanced_code_editor.dirty_grid_clear();
+
+   advanced_code_editor.cursor_move_to(14, 1);
+   advanced_code_editor.split_lines();
+
+
+
+
+   std::vector<std::pair<int, int>> expected_cells_from_current_line = {
+      { 14, 1 }, { 15, 1 }, { 16, 1 }, { 17, 1 }, { 18, 1 }, { 19, 1 }, { 20, 1 }, { 21, 1 }, { 22, 1 }, { 23, 1 },
+      { 24, 1 }, { 25, 1 },
+   };
+   std::vector<std::pair<int, int>> expected_cells_from_before_change = {
+      { 0, 2 }, { 1, 2 }, { 2, 2 },
+
+      { 0, 3 }, { 1, 3 }, { 2, 3 }, { 3, 3 }, { 4, 3 }, { 5, 3 }, { 6, 3 }, { 7, 3 }, { 8, 3 }, { 9, 3 }, { 10, 3 },
+      { 11, 3 }, { 12, 3 }, { 13, 3 }, { 14, 3 }, { 15, 3 }, { 16, 3 }, { 17, 3 },
+   };
+   std::vector<std::pair<int, int>> expected_cells_from_after_change = {
+      { 0, 2 }, { 1, 2 }, { 2, 2 }, { 3, 2 }, { 4, 2 }, { 5, 2 }, { 6, 2 }, { 7, 2 }, { 8, 2 }, { 9, 2 }, { 10, 2 },
+      { 11, 2 },
+
+      { 0, 3 }, { 1, 3 }, { 2, 3 },
+
+      { 0, 4 }, { 1, 4 }, { 2, 4 }, { 3, 4 }, { 4, 4 }, { 5, 4 }, { 6, 4 }, { 7, 4 }, { 8, 4 }, { 9, 4 }, { 10, 4 },
+      { 11, 4 }, { 12, 4 }, { 13, 4 }, { 14, 4 }, { 15, 4 }, { 16, 4 }, { 17, 4 },
+   };
+
+
+
+   std::vector<std::pair<int, int>> actual = advanced_code_editor.get_dirty_cells();
+   ASSERT_THAT(expected_cells_from_current_line, IsSubsetOf(actual));
+}
+
