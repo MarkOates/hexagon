eef37548486ff9116990df0e990e21c30a2e5311
Mark Oates <markoates0@gmail.com>
2020-08-02 11:38:52 -0400

    * Add test for #build_vector#

--- tests/Hexagon/DirtyGridTest.cpp
+++ tests/Hexagon/DirtyGridTest.cpp
@@ -23,3 +23,16 @@ TEST(Hexagon_DirtyGridTest, mark_as_dirty__will_include_the_item_in_the_list)
    ASSERT_EQ(2, dirty_cells.size());
 }
 
+TEST(Hexagon_DirtyGridTest, build_vector__will_return_a_vector_of_the_elements_in_dirty_cells)
+{
+   Hexagon::DirtyGrid dirty_grid;
+
+   dirty_grid.mark_as_dirty(3, 5);
+   dirty_grid.mark_as_dirty(7, 13);
+
+   std::vector<std::pair<int, int>> expected_dirty_cells_as_vector = { {3, 5}, { 7, 13} };
+   std::vector<std::pair<int, int>> actual_dirty_cells_as_vector = dirty_grid.build_vector();
+
+   ASSERT_EQ(expected_dirty_cells_as_vector, actual_dirty_cells_as_vector);
+}
+
