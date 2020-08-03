ba7e0222c00787c5d02612d1d92fb9ea952fac03
Mark Oates <markoates0@gmail.com>
2020-08-03 06:45:29 -0400

    * Add guard check on QuoreRenderer::render

--- src/Hexagon/Elements/QuoteRenderer.cpp
+++ src/Hexagon/Elements/QuoteRenderer.cpp
@@ -3,6 +3,8 @@
 #include <Hexagon/Elements/QuoteRenderer.hpp>
 #include <stdexcept>
 #include <sstream>
+#include <stdexcept>
+#include <sstream>
 
 
 namespace Hexagon
@@ -25,6 +27,18 @@ QuoteRenderer::~QuoteRenderer()
 
 void QuoteRenderer::render()
 {
+if (!(al_is_system_installed()))
+   {
+      std::stringstream error_message;
+      error_message << "QuoteRenderer" << "::" << "render" << ": error: " << "guard \"al_is_system_installed()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+if (!(al_is_font_addon_initialized()))
+   {
+      std::stringstream error_message;
+      error_message << "QuoteRenderer" << "::" << "render" << ": error: " << "guard \"al_is_font_addon_initialized()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
 return;
 
 }
--- tests/Hexagon/Elements/QuoteRendererTest.cpp
+++ tests/Hexagon/Elements/QuoteRendererTest.cpp
@@ -1,6 +1,11 @@
 
 #include <gtest/gtest.h>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { ASSERT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
 #include <Hexagon/Elements/QuoteRenderer.hpp>
 
 class Hexagon_Elements_QuoteRendererTestWithEmptyFixture : public ::testing::Test
@@ -16,9 +21,18 @@ TEST_F(Hexagon_Elements_QuoteRendererTestWithEmptyFixture, can_be_created_withou
    Hexagon::Elements::QuoteRenderer quote_renderer;
 }
 
+TEST_F(Hexagon_Elements_QuoteRendererTestWithEmptyFixture, render__without_allegro_initialized__raises_an_error)
+{
+   Hexagon::Elements::QuoteRenderer quote_renderer;
+   std::string expected_error_message =
+      "QuoteRenderer::render: error: guard \"al_is_system_installed()\" not met";
+   ASSERT_THROW_WITH_MESSAGE(quote_renderer.render(), std::runtime_error, expected_error_message);
+}
+
 TEST_F(Hexagon_Elements_QuoteRendererTestWithAllegroRenderingFixture, render__will_not_blow_up)
 {
    Hexagon::Elements::QuoteRenderer quote_renderer;
    quote_renderer.render();
    SUCCEED();
 }
+
