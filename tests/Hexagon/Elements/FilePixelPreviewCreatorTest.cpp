2d095f5967a931e7c722c111153c69ca2becf393
Mark Oates <mark.oates@shopify.com>
2020-08-05 12:37:25 -0400

    * Add rendering fixtures for FilePixelPreviewCreator

--- tests/Hexagon/Elements/FilePixelPreviewCreatorTest.cpp
+++ tests/Hexagon/Elements/FilePixelPreviewCreatorTest.cpp
@@ -3,12 +3,21 @@
 
 #include <Hexagon/Elements/FilePixelPreviewCreator.hpp>
 
-TEST(Hexagon_Elements_FilePixelPreviewCreatorTest, can_be_created_without_blowing_up)
+class Hexagon_Elements_FilePixelPreviewCreatorTestWithEmptyFixture : public ::testing::Test
+{};
+
+#include <Testing/WithAllegroRenderingFixture.hpp>
+
+class Hexagon_Elements_FilePixelPreviewCreatorTestWithAllegroRenderingFixture
+   : public Testing::WithAllegroRenderingFixture
+{};
+
+TEST_F(Hexagon_Elements_FilePixelPreviewCreatorTestWithEmptyFixture, can_be_created_without_blowing_up)
 {
    Hexagon::Elements::FilePixelPreviewCreator file_pixel_preview_creator;
 }
 
-TEST(Hexagon_Elements_FilePixelPreviewCreatorTest, create__does_not_blow_up)
+TEST_F(Hexagon_Elements_FilePixelPreviewCreatorTestWithAllegroRenderingFixture, create__does_not_blow_up)
 {
    Hexagon::Elements::FilePixelPreviewCreator file_pixel_preview_creator;
    file_pixel_preview_creator.create();
