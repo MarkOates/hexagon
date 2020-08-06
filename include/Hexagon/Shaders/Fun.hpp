de7ab67c33c09b5acc63b4a062524d890fd92d85
Mark Oates <mark.oates@shopify.com>
2020-08-06 19:09:00 -0400

    * Add Shaders/Fun

---
+++ include/Hexagon/Shaders/Fun.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace Shaders
+   {
+      class Fun
+      {
+      private:
+
+      public:
+         Fun();
+         ~Fun();
+
+
+      std::string run();
+      };
+   }
+}
+
+
+
---
+++ src/Hexagon/Shaders/Fun.cpp
@@ -0,0 +1,30 @@
+
+
+#include <Hexagon/Shaders/Fun.hpp>
+
+
+
+namespace Hexagon
+{
+namespace Shaders
+{
+
+
+Fun::Fun()
+{
+}
+
+
+Fun::~Fun()
+{
+}
+
+
+std::string Fun::run()
+{
+return "Hello World!";
+}
+} // namespace Shaders
+} // namespace Hexagon
+
+
