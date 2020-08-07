f41faf2bb4f042bb7ce6cfefb0cee4b29b249818
Mark Oates <mark.oates@shopify.com>
2020-08-06 20:54:57 -0400

    * Add in fixture with event queue

--- tests/Hexagon/Shaders/FunTest.cpp
+++ tests/Hexagon/Shaders/FunTest.cpp
@@ -9,9 +9,13 @@
 
 
 #include <Hexagon/Shaders/Fun.hpp>
+#include <AllegroFlare/FontBin.hpp>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_color.h>
 #include <allegro_flare/placement2d.h>
+#include <allegro_flare/placement3d.h>
 
 
 class Hexagon_Shaders_FunTest : public ::testing::Test
@@ -44,6 +48,56 @@ protected:
    ALLEGRO_DISPLAY *get_display()
    {
       return display;
+   }
+};
+
+
+class Hexagon_Shaders_FunTest_WithEventQueueFixture : public ::testing::Test
+{
+public:
+   ALLEGRO_DISPLAY* display;
+   AllegroFlare::FontBin font_bin;
+   ALLEGRO_EVENT_QUEUE *event_queue;
+
+public:
+   Hexagon_Shaders_FunTest_WithEventQueueFixture()
+      : display(nullptr)
+      , font_bin()
+      , event_queue(nullptr)
+   {}
+
+   virtual void SetUp() override
+   {
+      ASSERT_EQ(false, al_is_system_installed());
+      ASSERT_EQ(true, al_init());
+      al_init_primitives_addon();
+      al_init_font_addon();
+      al_init_ttf_addon();
+      al_install_keyboard();
+
+      event_queue = al_create_event_queue();
+      al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+      font_bin.set_full_path("/Users/markoates/Repos/hexagon/bin/programs/data/fonts");
+
+      display = al_create_display(1280 * 2, 720 * 2);
+      al_clear_to_color(ALLEGRO_COLOR{0.05f, 0.05f, 0.05f, 0.05f});
+   }
+
+   virtual void TearDown() override
+   {
+      font_bin.clear();
+      al_destroy_display(display);
+      al_destroy_event_queue(event_queue);
+      al_shutdown_ttf_addon(); // this is required otherwise subsequent al_init_ttf_addon will not work
+      al_uninstall_system();
+   }
+
+   placement3d centered_placement(float width, float height)
+   {
+      placement3d place(al_get_display_width(display)/2, al_get_display_height(display)/2, 0.0f);
+      place.size = vec3d(width, height, 0);
+      return place;
    }
 };
 
