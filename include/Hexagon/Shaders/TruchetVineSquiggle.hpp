ad91a2b54f8b060ebb50ad868dce752fb746b02d
Mark Oates <mark.oates@shopify.com>
2020-08-07 18:14:22 -0400

    * Copy current Shaders/Fun to Shaders/TruchetVineSquiggle

---
+++ include/Hexagon/Shaders/TruchetVineSquiggle.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+
+#include <Hexagon/Shaders/Base.hpp>
+#include <allegro5/allegro.h>
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace Shaders
+   {
+      class TruchetVineSquiggle : private Hexagon::Shaders::Base
+      {
+      private:
+         bool initialized;
+
+      public:
+         TruchetVineSquiggle();
+         ~TruchetVineSquiggle();
+
+
+      void initialize();
+      void activate();
+      void deactivate();
+      void set_flat_color(ALLEGRO_COLOR flat_color={});
+      void set_texture_width(int width=1);
+      void set_texture_height(int height=1);
+      void set_time(float time=1);
+      static std::string obtain_vertex_source();
+      static std::string obtain_fragment_source();
+      };
+   }
+}
+
+
+
