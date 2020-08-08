d2093b56e083f797adddff4db4a87c405992fdef
Mark Oates <markoates0@gmail.com>
2020-08-08 17:25:42 -0400

    * Remove dead code

--- tests/Hexagon/BlastComponentLayoutGeneratorTest.cpp
+++ tests/Hexagon/BlastComponentLayoutGeneratorTest.cpp
@@ -47,34 +47,26 @@ TEST(Hexagon_BlastComponentLayoutGeneratorTest, generate__creates_layouts_for_th
    Hexagon::Layout actual_layout = blast_project_layouts_generator.generate();
    std::vector<std::tuple<std::string, std::string, placement3d>> expected_layout_files, actual_layout_files;
 
-   int i=0;
-   //for (auto &expected_layout : expected_layouts)
-   //{
-      //Hexagon::Layout &actual_layout = actual_layouts[i];
+   EXPECT_EQ(expected_layout.get_concept_name(), actual_layout.get_concept_name());
 
-      EXPECT_EQ(expected_layout.get_concept_name(), actual_layout.get_concept_name());
+   expected_layout_files = expected_layout.get_files();
+   actual_layout_files = actual_layout.get_files();
 
-      expected_layout_files = expected_layout.get_files();
-      actual_layout_files = actual_layout.get_files();
+   ASSERT_EQ(expected_layout_files.size(), actual_layout_files.size());
 
-      ASSERT_EQ(expected_layout_files.size(), actual_layout_files.size());
+   int j=0;
+   for (auto &expected_layout_file : expected_layout_files)
+   {
+      std::tuple<std::string, std::string, placement3d> &actual_layout_file = actual_layout_files[j];
 
-      int j=0;
-      for (auto &expected_layout_file : expected_layout_files)
-      {
-         std::tuple<std::string, std::string, placement3d> &actual_layout_file = actual_layout_files[j];
+      // has the expected filename
+      EXPECT_EQ(std::get<0>(expected_layout_file), std::get<0>(actual_layout_file));
 
-         // has the expected filename
-         EXPECT_EQ(std::get<0>(expected_layout_file), std::get<0>(actual_layout_file));
+      // has the expected type
+      EXPECT_EQ(std::get<1>(expected_layout_file), std::get<1>(actual_layout_file));
 
-         // has the expected type
-         EXPECT_EQ(std::get<1>(expected_layout_file), std::get<1>(actual_layout_file));
-
-         j++;
-      }
-
-      //i++;
-   //}
+      j++;
+   }
 
    SUCCEED();
 }
