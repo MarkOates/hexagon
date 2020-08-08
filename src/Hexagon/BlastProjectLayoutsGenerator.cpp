98b636d840fc8cdc20019d757427a52d24461e7c
Mark Oates <markoates0@gmail.com>
2020-08-08 14:42:53 -0400

    * Add scaffold for #build_appropriate_layout_for_component#

--- src/Hexagon/BlastProjectLayoutsGenerator.cpp
+++ src/Hexagon/BlastProjectLayoutsGenerator.cpp
@@ -2,6 +2,8 @@
 
 #include <Hexagon/BlastProjectLayoutsGenerator.hpp>
 #include <Blast/Project/ComponentLister.hpp>
+#include <Blast/Project/Component.hpp>
+#include <iostream>
 
 
 namespace Hexagon
@@ -32,6 +34,30 @@ for (auto &component_name : component_names)
 }
 
 return result;
+
+}
+
+void BlastProjectLayoutsGenerator::build_appropriate_layout_for_component(std::string component_name)
+{
+Blast::Project::Component component(component_name, project_directory);
+
+if (component.has_only_source_and_header())
+{
+   //return create_three_split_from_last_component_navigator_selection();
+}
+else if (component.has_quintessence() || component.has_test())
+{
+   //return attempt_to_create_stage_from_last_component_navigator_selection();
+}
+else
+{
+   std::cout << "cannot build_appropriate_layout_for_component "
+             << "because the component \"" << component.get_name() << "\" "
+             << "neither has a header/source combo nor a quintessence or test file."
+             << std::endl;
+   return;
+}
+return;
 
 }
 } // namespace Hexagon
