da25e1a775479e3ce93037b42a842d3e194afe3a
Mark Oates <markoates0@gmail.com>
2020-08-08 17:04:44 -0400

    * Rename tested class name

--- tests/Hexagon/BlastComponentLayoutGeneratorTest.cpp
+++ tests/Hexagon/BlastComponentLayoutGeneratorTest.cpp
@@ -1,25 +1,25 @@
 
 #include <gtest/gtest.h>
 
-#include <Hexagon/BlastProjectLayoutsGenerator.hpp>
+#include <Hexagon/BlastComponentLayoutGenerator.hpp>
 
 const std::string FIXTURE_PROJECT_DIRECTORY = "/Users/markoates/Repos/hexagon/tests/fixtures/FixtureProject/";
 
-TEST(Hexagon_BlastProjectLayoutsGeneratorTest, can_be_created_without_blowing_up)
+TEST(Hexagon_BlastComponentLayoutGeneratorTest, can_be_created_without_blowing_up)
 {
-   Hexagon::BlastProjectLayoutsGenerator blast_project_layouts_generator;
+   Hexagon::BlastComponentLayoutGenerator blast_project_layouts_generator;
 }
 
-TEST(Hexagon_BlastProjectLayoutsGeneratorTest, generate__returns_the_expected_response)
+TEST(Hexagon_BlastComponentLayoutGeneratorTest, generate__returns_the_expected_response)
 {
-   Hexagon::BlastProjectLayoutsGenerator blast_project_layouts_generator(FIXTURE_PROJECT_DIRECTORY);
+   Hexagon::BlastComponentLayoutGenerator blast_project_layouts_generator(FIXTURE_PROJECT_DIRECTORY);
    blast_project_layouts_generator.generate();
    SUCCEED();
 }
 
-TEST(Hexagon_BlastProjectLayoutsGeneratorTest, generate__creates_layouts_for_the_project_components)
+TEST(Hexagon_BlastComponentLayoutGeneratorTest, generate__creates_layouts_for_the_project_components)
 {
-   Hexagon::BlastProjectLayoutsGenerator blast_project_layouts_generator(FIXTURE_PROJECT_DIRECTORY);
+   Hexagon::BlastComponentLayoutGenerator blast_project_layouts_generator(FIXTURE_PROJECT_DIRECTORY);
 
    std::vector<Hexagon::Layout> expected_layouts = {
       Hexagon::Layout("AnotherFixtureObject", {
