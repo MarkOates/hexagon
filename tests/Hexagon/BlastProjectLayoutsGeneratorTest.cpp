4db5d83aed09af71ca21b22cab569812f9863f47
Mark Oates <markoates0@gmail.com>
2020-08-08 14:15:53 -0400

    * Check layouts have been assigned the expected concept names

--- tests/Hexagon/BlastProjectLayoutsGeneratorTest.cpp
+++ tests/Hexagon/BlastProjectLayoutsGeneratorTest.cpp
@@ -20,9 +20,22 @@ TEST(Hexagon_BlastProjectLayoutsGeneratorTest, generate__returns_the_expected_re
 TEST(Hexagon_BlastProjectLayoutsGeneratorTest, generate__creates_layouts_for_the_project_components)
 {
    Hexagon::BlastProjectLayoutsGenerator blast_project_layouts_generator(FIXTURE_PROJECT_DIRECTORY);
-   std::vector<Hexagon::Layout> layouts = blast_project_layouts_generator.generate();
 
-   ASSERT_EQ(false, layouts.empty());
+   std::vector<Hexagon::Layout> expected_layouts = {
+      Hexagon::Layout("AnotherFixtureObject"),
+      Hexagon::Layout("FixtureObjectThing"),
+   };
+   std::vector<Hexagon::Layout> actual_layouts = blast_project_layouts_generator.generate();
+
+   ASSERT_EQ(false, actual_layouts.empty());
+
+   int i=0;
+   for (auto &expected_layout : expected_layouts)
+   {
+      Hexagon::Layout &actual_layout = actual_layouts[i];
+      ASSERT_EQ(expected_layout.get_concept_name(), actual_layout.get_concept_name());
+      i++;
+   }
 
    SUCCEED();
 }
