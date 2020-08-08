f2d2c703f18f57870e68a58025c932b5ca6f0f0d
Mark Oates <markoates0@gmail.com>
2020-08-08 18:14:04 -0400

    * Add test that two-split works

--- tests/Hexagon/BlastComponentLayoutGeneratorTest.cpp
+++ tests/Hexagon/BlastComponentLayoutGeneratorTest.cpp
@@ -17,7 +17,58 @@ TEST(Hexagon_BlastComponentLayoutGeneratorTest, generate__does_not_blow_up)
    SUCCEED();
 }
 
-TEST(Hexagon_BlastComponentLayoutGeneratorTest, generate__creates_layouts_for_the_project_components)
+TEST(Hexagon_BlastComponentLayoutGeneratorTest,
+   generate__creates_a_two_split_layout_for_components_that_have_a_quintessence)
+{
+   Hexagon::BlastComponentLayoutGenerator blast_project_layouts_generator(
+      FIXTURE_PROJECT_DIRECTORY,
+      "FixtureObjectThing"
+   );
+
+   Hexagon::Layout expected_layout = {
+      "FixtureObjectThing",
+      {
+         {
+            "/Users/markoates/Repos/hexagon/tests/fixtures/FixtureProject/quintessence/FixtureObjectThing.q.yml",
+            "blast_quintessence",
+            {},
+         },
+         {
+            "/Users/markoates/Repos/hexagon/tests/fixtures/FixtureProject/tests/FixtureObjectThingTest.cpp",
+            "blast_test",
+            {},
+         },
+      },
+   };
+   Hexagon::Layout actual_layout = blast_project_layouts_generator.generate();
+   std::vector<std::tuple<std::string, std::string, placement3d>> expected_layout_files, actual_layout_files;
+
+   EXPECT_EQ(expected_layout.get_concept_name(), actual_layout.get_concept_name());
+
+   expected_layout_files = expected_layout.get_files();
+   actual_layout_files = actual_layout.get_files();
+
+   ASSERT_EQ(expected_layout_files.size(), actual_layout_files.size());
+
+   int j=0;
+   for (auto &expected_layout_file : expected_layout_files)
+   {
+      std::tuple<std::string, std::string, placement3d> &actual_layout_file = actual_layout_files[j];
+
+      // has the expected filename
+      EXPECT_EQ(std::get<0>(expected_layout_file), std::get<0>(actual_layout_file));
+
+      // has the expected type
+      EXPECT_EQ(std::get<1>(expected_layout_file), std::get<1>(actual_layout_file));
+
+      j++;
+   }
+
+   SUCCEED();
+}
+
+TEST(Hexagon_BlastComponentLayoutGeneratorTest,
+   generate__creates_a_three_split_layout_for_components_that_only_have_a_header_and_source)
 {
    Hexagon::BlastComponentLayoutGenerator blast_project_layouts_generator(
       FIXTURE_PROJECT_DIRECTORY,
