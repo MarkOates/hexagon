5aca366dbba11ad97bbff443fdf08fd3a03a3367
Mark Oates <markoates0@gmail.com>
2020-08-09 14:53:15 -0400

    * Add second test case for for clipping

--- tests/Hexagon/Elements/BitmapGridMeshTest.cpp
+++ tests/Hexagon/Elements/BitmapGridMeshTest.cpp
@@ -420,7 +420,7 @@ TEST_F(Hexagon_Elements_BitmapGridMeshTest_WithAllegroRenderingFixture, render__
    std::vector<std::tuple<int, int, int, ALLEGRO_COLOR>> coordinates_to_check =
       {
          { num_rows, 870, 670, al_color_name("yellow") }, // bottom right
-         //{ num_rows-1, 870, 670, al_color_name("black") },
+         { num_rows-1, 870, 670, al_color_name("black") },
       };
 
    for (auto &coordinate_to_check : coordinates_to_check)
@@ -441,8 +441,6 @@ TEST_F(Hexagon_Elements_BitmapGridMeshTest_WithAllegroRenderingFixture, render__
       EXPECT_EQ_COLOR(expected_pixel_color, actual_color);
 
       al_flip_display();
-
-      sleep(2);
    }
 }
 
