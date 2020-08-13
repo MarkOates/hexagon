70492df4e762f82c73f16415840037d289876344
Mark Oates <markoates0@gmail.com>
2020-08-13 18:07:38 -0400

    * Save current passing state

--- src/Hexagon/LayoutFromYAML.cpp
+++ src/Hexagon/LayoutFromYAML.cpp
@@ -24,14 +24,28 @@ Hexagon::Layout LayoutFromYAML::load()
 {
 // https://github.com/jbeder/yaml-cpp/wiki/Tutorial
 
+
+// create the root node
 YAML::Node node = YAML::Load(source_yaml);
 
+
+// parse concept name
 std::string concept_name = node["name"].as<std::string>();
 
 YAML::Node files_node = node["files"];
-std::vector<std::tuple<std::string, std::string, placement3d>> files = {};
 
-Hexagon::Layout result(concept_name);
+
+// parse files
+std::vector<std::tuple<std::string, std::string, placement3d>> result_files = {};
+for (std::size_t i=0;i<files_node.size();i++)
+{
+}
+
+std::string daemus_command = ""; // node["daemus"].as<std::string>();
+
+
+// build the object
+Hexagon::Layout result(concept_name, result_files, daemus_command);
 
 return result;
 
