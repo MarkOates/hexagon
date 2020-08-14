400963ca62176a4d77d4eb20ffaf60e2b6b0fe36
Mark Oates <markoates0@gmail.com>
2020-08-14 00:05:46 -0400

    * Activate assertion in test

--- tests/Hexagon/ProjectFromYAMLTest.cpp
+++ tests/Hexagon/ProjectFromYAMLTest.cpp
@@ -42,6 +42,6 @@ TEST(Hexagon_ProjectFromYAMLTest, load__returns_a_project_with_the_expected_valu
    Hexagon::Project expected_project;
    Hexagon::Project actual_project = project_from_yaml.load();
 
-   //ASSERT_EQ(expected_project.get_project_name(), actual_project.get_project_name());
+   ASSERT_EQ(expected_project.get_project_name(), actual_project.get_project_name());
 }
 
