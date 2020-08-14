82e180edb421e921703e4b051a76d131c856291e
Mark Oates <markoates0@gmail.com>
2020-08-13 23:44:54 -0400

    * Add unfinished test for #load# method

--- tests/Hexagon/ProjectFromYAMLTest.cpp
+++ tests/Hexagon/ProjectFromYAMLTest.cpp
@@ -3,6 +3,9 @@
 
 #include <Hexagon/ProjectFromYAML.hpp>
 
+std::string FULL_TEST_FIXTURE = R"END(
+)END";
+
 TEST(Hexagon_ProjectFromYAMLTest, can_be_created_without_blowing_up)
 {
    Hexagon::ProjectFromYAML project_from_yaml;
@@ -13,5 +16,15 @@ TEST(Hexagon_ProjectFromYAMLTest, load__returns_the_expected_response)
    Hexagon::ProjectFromYAML project_from_yaml;
    project_from_yaml.load();
    SUCCEED();
+}
+
+TEST(Hexagon_ProjectFromYAMLTest, load__returns_a_project_with_the_expected_values)
+{
+   Hexagon::ProjectFromYAML project_from_yaml(FULL_TEST_FIXTURE);
+
+   Hexagon::Project expected_project;
+   Hexagon::Project actual_project = project_from_yaml.load();
+
+   //EXPECT_EQ(expected_project, actual_project);
 }
 
