afb7e010807a92d4381cc43d818385248136ad64
Mark Oates <markoates0@gmail.com>
2020-08-14 21:29:20 -0400

    * Set a decent background color for this thing

--- tests/Hexagon/Elements/DynamicGridMeshTest.cpp
+++ tests/Hexagon/Elements/DynamicGridMeshTest.cpp
@@ -71,7 +71,7 @@ TEST_F(Hexagon_Elements_DynamicGridMeshTest_WithEmptyFixture, can_be_created_wit
 
 TEST_F(Hexagon_Elements_DynamicGridMeshTest_WithAllegroRenderingFixture, render__draws_the_mesh)
 {
-   al_clear_to_color(al_color_name("pink"));
+   al_clear_to_color(al_color_name("saddlebrown"));
    al_flip_display();
    sleep(2);
 }
