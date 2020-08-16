9c23428e82f2d5a7af28ae440c726b1f3068357f
Mark Oates <markoates0@gmail.com>
2020-08-16 03:07:29 -0400

    Add Hexagon/CodeMessagePointTest test file

---
+++ tests/Hexagon/CodeMessagePointTest.cpp
@@ -0,0 +1,10 @@
+
+#include <gtest/gtest.h>
+
+#include <Hexagon/CodeMessagePoint.hpp>
+
+TEST(Hexagon_CodeMessagePointTest, can_be_created_without_blowing_up)
+{
+   Hexagon::CodeMessagePoint code_message_point;
+}
+
