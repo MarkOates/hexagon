2514426b8c8f84cf1fc0b3295d98080041fad7d5
Mark Oates <markoates0@gmail.com>
2020-08-16 02:33:32 -0400

    * Add scaffold for CodeMessagePointManager

---
+++ include/Hexagon/CodeMessagePointManager.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Hexagon
+{
+   class CodeMessagePointManager
+   {
+   private:
+
+   public:
+      CodeMessagePointManager();
+      ~CodeMessagePointManager();
+
+
+   std::string run();
+   };
+}
+
+
+
---
+++ src/Hexagon/CodeMessagePointManager.cpp
@@ -0,0 +1,27 @@
+
+
+#include <Hexagon/CodeMessagePointManager.hpp>
+
+
+
+namespace Hexagon
+{
+
+
+CodeMessagePointManager::CodeMessagePointManager()
+{
+}
+
+
+CodeMessagePointManager::~CodeMessagePointManager()
+{
+}
+
+
+std::string CodeMessagePointManager::run()
+{
+return "Hello World!";
+}
+} // namespace Hexagon
+
+
