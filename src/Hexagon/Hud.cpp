59763da8e7fd550158579a7fe831337da8b7541d
Mark Oates <markoates0@gmail.com>
2020-08-16 01:57:58 -0400

    * Set num packets to 10 to include the in-progress packet

--- src/Hexagon/Hud.cpp
+++ src/Hexagon/Hud.cpp
@@ -566,8 +566,8 @@ if (draw_packets)
    std::vector<Hexagon::Packet> packets_to_render = packets;
    packets_to_render.push_back(packet_in_progress);
 
-   // 9, because golf has 9-hole halves, also divisible by 2 or 3. Good number
-   int num_packets_to_render = std::min(9, (int)(packets_to_render.size() - 1));
+   // 9, because golf has 9-hole halves, also divisible by 2 or 3. Good number.  Also 1 extra for the in-progress
+   int num_packets_to_render = std::min(10, (int)(packets_to_render.size() - 1));
 
    for (int i=num_packets_to_render; i>=0; i--)
    {
