797ab59876ac1a512ea78e94618b4d641a243b2a
Mark Oates <markoates0@gmail.com>
2020-08-16 22:18:10 -0400

    * Add #x# and #y# properties

--- include/Hexagon/Elements/ListMenu.hpp
+++ include/Hexagon/Elements/ListMenu.hpp
@@ -19,6 +19,8 @@ namespace Hexagon
          std::string title;
          std::vector<std::tuple<std::string, std::string>> list_items;
          int cursor;
+         int x;
+         int y;
 
       public:
          ListMenu(AllegroFlare::FontBin* font_bin=nullptr, std::string title="+", std::vector<std::tuple<std::string, std::string>> list_items={});
