556454f1bed50c2423d3cec8076cdf7a4f9e5b3a
Mark Oates <mark.oates@shopify.com>
2020-08-17 09:55:16 -0400

    Fix failing test for expecting packets to be off by default

--- tests/Hexagon/HudTest.cpp
+++ tests/Hexagon/HudTest.cpp
@@ -97,7 +97,7 @@ TEST_F(Hexagon_Elements_HudTest_WithEmptyFixture,
    draw_packets__has_a_getter_and_is_initialized_with_the_expected_default)
 {
    Hexagon::Hud hud;
-   ASSERT_EQ(true, hud.get_draw_packets());
+   ASSERT_EQ(false, hud.get_draw_packets());
 }
 
 TEST_F(Hexagon_Elements_HudTest_WithEmptyFixture,
