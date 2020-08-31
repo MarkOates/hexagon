97d10d325d9242858f31ecaead5ef771bc2cff0f
Mark Oates <markoates0@gmail.com>
2020-08-31 19:20:01 -0400

    * Add untested #build_component_navigator_initial_place#

--- include/Hexagon/StageFactory.hpp
+++ include/Hexagon/StageFactory.hpp
@@ -2,6 +2,8 @@
 
 
 #include <Hexagon/StageInterface.hpp>
+#include <allegro5/allegro.h>
+#include <allegro_flare/placement3d.h>
 
 
 namespace Hexagon
@@ -15,6 +17,8 @@ namespace Hexagon
       ~StageFactory();
 
 
+   ALLEGRO_DISPLAY* get_current_display();
+   placement3d build_component_navigator_initial_place();
    StageInterface* create();
    };
 }
--- src/Hexagon/StageFactory.cpp
+++ src/Hexagon/StageFactory.cpp
@@ -1,7 +1,9 @@
 
 
 #include <Hexagon/StageFactory.hpp>
-
+#include <allegro5/allegro.h>
+#include <stdexcept>
+#include <sstream>
 
 
 namespace Hexagon
@@ -17,6 +19,37 @@ StageFactory::~StageFactory()
 {
 }
 
+
+ALLEGRO_DISPLAY* StageFactory::get_current_display()
+{
+if (!(al_is_system_installed()))
+   {
+      std::stringstream error_message;
+      error_message << "StageFactory" << "::" << "get_current_display" << ": error: " << "guard \"al_is_system_installed()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+if (!(al_get_current_display()))
+   {
+      std::stringstream error_message;
+      error_message << "StageFactory" << "::" << "get_current_display" << ": error: " << "guard \"al_get_current_display()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+return al_get_current_display();
+
+}
+
+placement3d StageFactory::build_component_navigator_initial_place()
+{
+ALLEGRO_DISPLAY *display = get_current_display();
+placement3d result;
+result.position = vec3d(al_get_display_width(display)/2, al_get_display_height(display)/2, 0);
+result.size = vec3d(800, 700, 0);
+result.align = vec3d(0.5, 0.5, 0.5);
+result.scale = vec3d(1.0, 1.0, 1.0);
+result.rotation = vec3d(0.0, 0.0, 0.0);
+return result;
+
+}
 
 StageInterface* StageFactory::create()
 {
