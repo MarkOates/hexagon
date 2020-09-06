0894f3ecb1a4d9bc38287e27213f40359fad6574
Mark Oates <markoates0@gmail.com>
2020-09-06 00:38:04 -0400

    * Use factory method to create rerun_output_watcher in System/System

--- src/Hexagon/System/System.cpp
+++ src/Hexagon/System/System.cpp
@@ -1035,16 +1035,11 @@ bool System::spawn_file_navigator_from_last_file_navigator_folder_selection()
 
 bool System::spawn_rerun_output_watcher()
 {
-   float golden_ratio = 0.61803f;
-   float x_pos = golden_ratio * get_display_default_width() - get_display_default_width()/2;
-   placement3d place(x_pos, -get_display_default_height()/2 + 30, 0);
-   place.size = vec3d(0, 0, 0.0);
-   place.align = vec3d(0, 0, 0);
-   place.scale = vec3d(0.7, 0.7, 0.0);
+   Hexagon::StageFactory stage_factory(&config, &font_bin);
+   StageInterface *stage = stage_factory.create_rerun_output_watcher();
 
-   Hexagon::RerunOutputWatcher::Stage *rerun_output_watcher = new Hexagon::RerunOutputWatcher::Stage();
-   rerun_output_watcher->set_place(place);
-   stages.push_back(rerun_output_watcher);
+   stages.push_back(stage);
+
    return true;
 }
 
