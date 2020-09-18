c2d6d4105ccb073e82c896ba3e837482e7aae673
Mark Oates <markoates0@gmail.com>
2020-09-17 22:31:06 -0400

    * Add test for #stages# guard in #all# method

--- tests/Hexagon/StageCollectionHelperTest.cpp
+++ tests/Hexagon/StageCollectionHelperTest.cpp
@@ -1,11 +1,25 @@
 
 #include <gtest/gtest.h>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
 #include <Hexagon/StageCollectionHelper.hpp>
+
+void foo() {};
 
 TEST(Hexagon_StageCollectionHelperTest, can_be_created_without_blowing_up)
 {
    Hexagon::StageCollectionHelper stage_collection_helper;
+}
+
+TEST(Hexagon_StageCollectionHelperTest, all__without_stages__raises_an_exception)
+{
+   Hexagon::StageCollectionHelper stage_collection_helper;
+   std::string expected_error_message = "StageCollectionHelper::all: error: guard \"stages\" not met";
+   ASSERT_THROW_WITH_MESSAGE(stage_collection_helper.all(), std::runtime_error, expected_error_message);
 }
 
 TEST(Hexagon_StageCollectionHelperTest, all__returns_all_of_the_stages)
