4bcd0b68641251613d26cca657bae6be5218ad03
Mark Oates <markoates0@gmail.com>
2020-09-17 22:06:39 -0400

    * Add blank line to end of test file for Hexagon/StageCollectionHelper

--- tests/Hexagon/StageCollectionHelperTest.cpp
+++ tests/Hexagon/StageCollectionHelperTest.cpp
@@ -14,3 +14,4 @@ TEST(Hexagon_StageCollectionHelperTest, run__returns_the_expected_response)
    std::string expected_string = "Hello World!";
    EXPECT_EQ(expected_string, stage_collection_helper.run());
 }
+
