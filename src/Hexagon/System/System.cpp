62376c7a565ade9362b37e234c5dae86436843df
Mark Oates <markoates0@gmail.com>
2021-03-02 22:04:31 -0500

    Revert "* Remove skew on search box spawn"
    This reverts commit a7124d6b0c86121dc850fef2c2822bb30ed1170e.

--- src/Hexagon/System/System.cpp
+++ src/Hexagon/System/System.cpp
@@ -910,6 +910,9 @@ bool System::spawn_regex_input_box_modal()
 
    stages.push_back(stage);
 
+   motion.cmove_to(&camera.rotation.x, 0.08f, 0.5f, interpolator::tripple_fast_in);
+   motion.cmove_to(&camera.position.y, -70.0f, 0.5f, interpolator::tripple_fast_in);
+
    return true;
 }
 
