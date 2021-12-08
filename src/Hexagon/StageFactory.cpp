11a05c39c90eb4e4cc62d052b415ad3a50680dba
Mark Oates <markoates0@gmail.com>
2021-12-08 15:48:09 -0500

    * Adjust position of class brief menu in Hexagon/StageFactory

--- src/Hexagon/StageFactory.cpp
+++ src/Hexagon/StageFactory.cpp
@@ -210,7 +210,7 @@ StageInterface* StageFactory::create_class_brief_menu(std::string title, std::ve
    Hexagon::UI::LittleMenu *little_menu = new Hexagon::UI::LittleMenu(font_bin, title, items);
    little_menu->initialize();
    little_menu->set_render_on_hud(true);
-   little_menu->set_place(build_centered_on_hud_initial_place(400, 300));
+   little_menu->set_place(build_centered_on_hud_initial_place(0, 300));
    return little_menu;
 }
 
