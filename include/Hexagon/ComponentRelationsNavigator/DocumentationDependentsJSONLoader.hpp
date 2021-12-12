17f9910d994c3c1e0a29c3ba0be37a4f6eaec925
Mark Oates <markoates0@gmail.com>
2021-12-11 23:28:21 -0500

    * Add internal logic to verify dependents json file and contents

--- include/Hexagon/ComponentRelationsNavigator/DocumentationDependentsJSONLoader.hpp
+++ include/Hexagon/ComponentRelationsNavigator/DocumentationDependentsJSONLoader.hpp
@@ -20,6 +20,8 @@ namespace Hexagon
 
          std::string get_DEPENDENTS_JSON_FILENAME();
          std::vector<std::string> build_dependent_names_for_component_name(std::string component_name="");
+         std::string dependents_json_file_contents();
+         bool dependents_json_file_exists();
       };
    }
 }
--- src/Hexagon/ComponentRelationsNavigator/DocumentationDependentsJSONLoader.cpp
+++ src/Hexagon/ComponentRelationsNavigator/DocumentationDependentsJSONLoader.cpp
@@ -2,6 +2,11 @@
 
 #include <Hexagon/ComponentRelationsNavigator/DocumentationDependentsJSONLoader.hpp>
 #include <lib/nlohmann/json.hpp>
+#include <Blast/FileExistenceChecker.hpp>
+#include <allegro_flare/useful_php.h>
+#include <stdexcept>
+#include <sstream>
+#include <Blast/FileExistenceChecker.hpp>
 
 
 namespace Hexagon
@@ -29,8 +34,25 @@ std::string DocumentationDependentsJSONLoader::get_DEPENDENTS_JSON_FILENAME()
 
 std::vector<std::string> DocumentationDependentsJSONLoader::build_dependent_names_for_component_name(std::string component_name)
 {
-   nlohmann::json j;
+   nlohmann::json j(dependents_json_file_contents());
+
    return {};
+}
+
+std::string DocumentationDependentsJSONLoader::dependents_json_file_contents()
+{
+   if (!(dependents_json_file_exists()))
+      {
+         std::stringstream error_message;
+         error_message << "DocumentationDependentsJSONLoader" << "::" << "dependents_json_file_contents" << ": error: " << "guard \"dependents_json_file_exists()\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   return php::file_get_contents(get_DEPENDENTS_JSON_FILENAME());
+}
+
+bool DocumentationDependentsJSONLoader::dependents_json_file_exists()
+{
+   return Blast::FileExistenceChecker(get_DEPENDENTS_JSON_FILENAME()).exists();
 }
 } // namespace ComponentRelationsNavigator
 } // namespace Hexagon
