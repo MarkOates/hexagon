3ad74491515026ea638ed29d4150ebbe734a9f55
Mark Oates <markoates0@gmail.com>
2021-12-17 23:48:49 -0500

    * Add a neat little #draw_grid# helper function

--- tests/Hexagon/CameraTest.cpp
+++ tests/Hexagon/CameraTest.cpp
@@ -3,6 +3,27 @@
 
 #include <Hexagon/Camera.hpp>
 
+#include <allegro5/allegro_primitives.h>
+
+
+void draw_grid(float width, float height, float spacing)
+{
+   ALLEGRO_COLOR line_color = ALLEGRO_COLOR{0.2, 0.2, 0.2, 0.2};
+   float thickness = 1.0f;
+
+   for (unsigned y=0; y<=height; y+=spacing)
+   {
+      // horizontal lines
+      al_draw_line(0, y, width, y, line_color, thickness);
+   }
+
+   for (unsigned x=0; x<=width; x+=spacing)
+   {
+      // vertical lines
+      al_draw_line(x, 0, x, height, line_color, thickness);
+   }
+}
+
 
 class Hexagon_CameraTest_WithEmptyFixture : public ::testing::Test
 {
@@ -30,8 +51,9 @@ public:
       //ASSERT_EQ(true, al_init_primitives_addon());
       //ASSERT_EQ(true, al_init_font_addon());
       //ASSERT_EQ(true, al_init_ttf_addon());
-      display = al_create_display(1280*2, 720*2);
+      //display = al_create_display(1280*2, 720*2);
       //display = al_create_display(1920 / 2 * 4, 1080 / 2 * 4);
+      display = al_create_display(1920 * 2, 1080 * 2);
       //font_bin.set_full_path("/Users/markoates/Repos/hexagon/bin/programs/data/fonts");
       al_clear_to_color(ALLEGRO_COLOR{0.0f, 0.0f, 0.0f, 0.0f});
    }
@@ -64,5 +86,13 @@ TEST_F(Hexagon_CameraTest_WithAllegroRenderingFixture, setup_camera_perspective_
    ALLEGRO_BITMAP *bitmap = al_get_backbuffer(display);
    Hexagon::Camera camera;
    camera.setup_camera_perspective(bitmap);
+
+   draw_grid(500, 500, 50);
+   draw_grid(500, 500, 500);
+   draw_grid(500, 500, 100);
+
+   al_flip_display();
+
+   //sleep(2);
 }
 
