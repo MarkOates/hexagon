97084f5dbf6c90df8c518687b34e817317e3ec1e
Mark Oates <markoates0@gmail.com>
2022-01-11 09:41:55 -0500

    * Add some blank spaces

--- src/Blast/Daemus/NetworkMessageEventController.cpp
+++ src/Blast/Daemus/NetworkMessageEventController.cpp
@@ -24,9 +24,7 @@ NetworkMessageEventController::~NetworkMessageEventController()
 bool NetworkMessageEventController::process_message(std::string message)
 {
    bool message_captured = false;
-
    if (message == "TO_DAEMUS: OUTPUT_PID") { daemus->output_pid(); message_captured = true; }
-
    return message_captured;
 }
 } // namespace Daemus
