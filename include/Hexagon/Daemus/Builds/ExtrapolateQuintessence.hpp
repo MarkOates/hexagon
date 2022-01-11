bc563259a43f3cf191b46ebf250c5f9cf5a32952
Mark Oates <markoates0@gmail.com>
2022-01-11 17:43:26 -0500

    * Add some operational build file functionality to ExtrapolateQuintessence build

--- include/Hexagon/Daemus/Builds/ExtrapolateQuintessence.hpp
+++ include/Hexagon/Daemus/Builds/ExtrapolateQuintessence.hpp
@@ -2,6 +2,7 @@
 
 
 #include <Hexagon/Daemus/Builds/Base.hpp>
+#include <string>
 
 
 namespace Hexagon
@@ -13,12 +14,20 @@ namespace Hexagon
          class ExtrapolateQuintessence : public Hexagon::Daemus::Builds::Base
          {
          private:
+            std::string BUILDS_PENDING_FOLDER_LOCATION;
 
          public:
             ExtrapolateQuintessence();
             virtual ~ExtrapolateQuintessence();
 
+            void set_BUILDS_PENDING_FOLDER_LOCATION(std::string BUILDS_PENDING_FOLDER_LOCATION);
+            std::string get_BUILDS_PENDING_FOLDER_LOCATION();
             virtual void initiate() override;
+            void write_build_file();
+            void poll_for_progress();
+            std::string generate_build_file_contents();
+            std::string generate_build_filename();
+            static std::string generate_time_now_string();
          };
       }
    }
