3811c950e3a7c3d43833067440017e5da52a83d7
Mark Oates <markoates0@gmail.com>
2022-01-16 16:52:54 -0500

    * Add test file for Hexagon/StageInterface

---
+++ tests/Hexagon/StageInterfaceTest.cpp
@@ -0,0 +1,11 @@
+
+#include <gtest/gtest.h>
+
+#include <Hexagon/StageInterface.hpp>
+
+
+TEST(Hexagon_StageInterfaceTest, DISABLED__can_be_created_without_blowing_up)
+{
+   // TODO: this file requires a derived class
+   //StageInterface stage_interface;
+}
