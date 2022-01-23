3bd80af944b50e4bdaad2fb1dee4a51efb9bbf9e
Mark Oates <markoates0@gmail.com>
2022-01-23 11:48:11 -0500

    * Add #objectives_log# and #cursor_pos# property to ObjectivesLog/Stage

--- include/Hexagon/ObjectivesLog/Stage.hpp
+++ include/Hexagon/ObjectivesLog/Stage.hpp
@@ -2,6 +2,7 @@
 
 
 #include <Hexagon/ActionData.hpp>
+#include <Hexagon/ObjectivesLog/ObjectivesLog.hpp>
 #include <Hexagon/StageInterface.hpp>
 #include <allegro5/allegro.h>
 #include <string>
@@ -15,13 +16,18 @@ namespace Hexagon
       {
       private:
          static ALLEGRO_EVENT a_default_empty_event;
+         Hexagon::ObjectivesLog::ObjectivesLog* objectives_log;
+         int cursor_pos;
 
       public:
          Stage();
          virtual ~Stage();
 
          static ALLEGRO_EVENT &get_a_default_empty_event_ref();
+         Hexagon::ObjectivesLog::ObjectivesLog* &get_objectives_log_ref();
          virtual void render() override;
+         void move_cursor_up();
+         int infer_num_list_items();
          virtual void process_local_event(std::string event_name="", ActionData action_data=ActionData()) override;
          virtual void process_event(ALLEGRO_EVENT& event=get_a_default_empty_event_ref()) override;
       };
--- src/Hexagon/ObjectivesLog/Stage.cpp
+++ src/Hexagon/ObjectivesLog/Stage.cpp
@@ -1,7 +1,8 @@
 
 
 #include <Hexagon/ObjectivesLog/Stage.hpp>
-
+#include <stdexcept>
+#include <sstream>
 
 
 namespace Hexagon
@@ -15,6 +16,8 @@ ALLEGRO_EVENT Stage::a_default_empty_event = {};
 
 Stage::Stage()
    : StageInterface(StageInterface::OBJECTIVES_LOG)
+   , objectives_log()
+   , cursor_pos(0)
 {
 }
 
@@ -30,9 +33,33 @@ ALLEGRO_EVENT &Stage::get_a_default_empty_event_ref()
 }
 
 
+Hexagon::ObjectivesLog::ObjectivesLog* &Stage::get_objectives_log_ref()
+{
+   return objectives_log;
+}
+
+
 void Stage::render()
 {
    return;
+}
+
+void Stage::move_cursor_up()
+{
+   cursor_pos--;
+   if (cursor_pos < 0) cursor_pos = infer_num_list_items() - 1;
+   return;
+}
+
+int Stage::infer_num_list_items()
+{
+   if (!(objectives_log))
+      {
+         std::stringstream error_message;
+         error_message << "Stage" << "::" << "infer_num_list_items" << ": error: " << "guard \"objectives_log\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   return objectives_log->get_objectives_ref().size();
 }
 
 void Stage::process_local_event(std::string event_name, ActionData action_data)
