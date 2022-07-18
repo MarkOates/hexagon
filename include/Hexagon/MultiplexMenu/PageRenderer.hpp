be7c1f3c7b5c1544a5cddc50542ceaa1613e94ef
Mark Oates <markoates0@gmail.com>
2022-07-18 17:51:17 -0400

    * Add basic duplicated code for PageRenderer

---
+++ include/Hexagon/MultiplexMenu/PageRenderer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+
+#include <AllegroFlare/FontBin.hpp>
+#include <Hexagon/MultiplexMenu/MultiplexMenu.hpp>
+#include <allegro5/allegro_font.h>
+#include <string>
+
+
+namespace Hexagon
+{
+   namespace MultiplexMenu
+   {
+      class PageRenderer
+      {
+      private:
+         AllegroFlare::FontBin* font_bin;
+         Hexagon::MultiplexMenu::MultiplexMenuPage* page;
+         float width;
+         float height;
+
+      public:
+         PageRenderer(AllegroFlare::FontBin* font_bin=nullptr, Hexagon::MultiplexMenu::MultiplexMenuPage* page=nullptr, float width=1, float height=1);
+         ~PageRenderer();
+
+         void render();
+         void render_page();
+         void render_menu_item(float x=0, float y=0, std::string input="^H", std::string label="Unlabeled", bool opens_menu=false, std::string state="no_state");
+         static std::string convert_key_input_to_string(int al_keycode=0, bool ctrl=false, bool alt=false, bool command=false, bool shift=false);
+         ALLEGRO_FONT* obtain_font();
+         ALLEGRO_FONT* obtain_keyboard_key_font();
+      };
+   }
+}
+
+
+
---
+++ tests/Hexagon/MultiplexMenu/PageRendererTest.cpp
@@ -0,0 +1,88 @@
+
+#include <gtest/gtest.h>
+
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { ASSERT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <Testing/WithAllegroRenderingFixture.hpp>
+
+class Hexagon_MultiplexMenu_PageRendererTest : public ::testing::Test
+{};
+
+class Hexagon_MultiplexMenu_PageRendererTestWithAllegroRenderingFixture : public Testing::WithAllegroRenderingFixture
+{};
+
+
+#include <Hexagon/MultiplexMenu/PageRenderer.hpp>
+
+
+TEST_F(Hexagon_MultiplexMenu_PageRendererTest, can_be_created_without_blowing_up)
+{
+   Hexagon::MultiplexMenu::PageRenderer renderer;
+}
+
+
+TEST_F(Hexagon_MultiplexMenu_PageRendererTest, render__without_allegro_initialized__raises_an_error)
+{
+   Hexagon::MultiplexMenu::PageRenderer renderer;
+   std::string expected_error_message =
+      "PageRenderer::render: error: guard \"al_is_system_installed()\" not met";
+   ASSERT_THROW_WITH_MESSAGE(renderer.render(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(Hexagon_MultiplexMenu_PageRendererTestWithAllegroRenderingFixture, render__will_render_as_expected)
+{
+   al_install_keyboard();
+   Hexagon::MultiplexMenu::PageRenderer renderer(&get_font_bin_ref());
+   al_clear_to_color(ALLEGRO_COLOR{0.1, 0.1, 0.1, 1});
+
+   renderer.render();
+
+   al_flip_display();
+   sleep(1);
+
+   al_uninstall_keyboard();
+   SUCCEED();
+}
+
+
+TEST_F(Hexagon_MultiplexMenu_PageRendererTest,
+   convert_key_input_to_string__without_allegro_initialized__will_throw_an_error)
+{
+   Hexagon::MultiplexMenu::PageRenderer renderer;
+   std::string expected_error_message =
+      "PageRenderer::convert_key_input_to_string: error: guard \"al_is_system_installed()\" not met";
+   ASSERT_THROW_WITH_MESSAGE(renderer.convert_key_input_to_string(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(Hexagon_MultiplexMenu_PageRendererTest,
+   convert_key_input_to_string__without_a_keyboard_initialized__will_throw_an_error)
+{
+   al_init();
+   Hexagon::MultiplexMenu::PageRenderer renderer;
+   std::string expected_error_message =
+      "PageRenderer::convert_key_input_to_string: error: guard \"al_is_keyboard_installed()\" not met";
+   ASSERT_THROW_WITH_MESSAGE(renderer.convert_key_input_to_string(), std::runtime_error, expected_error_message);
+   al_uninstall_system();
+}
+
+
+TEST_F(Hexagon_MultiplexMenu_PageRendererTest,
+   convert_key_input_to_string__will_return_a_string_representation_of_the_keyboard_input)
+{
+   al_init();
+   al_install_keyboard();
+   Hexagon::MultiplexMenu::PageRenderer renderer;
+
+   EXPECT_EQ("Shift+N", renderer.convert_key_input_to_string(ALLEGRO_KEY_N, false, false, false, true));
+   EXPECT_EQ("Ctrl+Shift+DOWN", renderer.convert_key_input_to_string(ALLEGRO_KEY_DOWN, true, false, false, true));
+   EXPECT_EQ("Command+SPACE", renderer.convert_key_input_to_string(ALLEGRO_KEY_SPACE, false, false, true, false));
+
+   al_uninstall_system();
+}
+
+
