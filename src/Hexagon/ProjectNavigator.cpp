fe24df0af995bfefa7a2a10d8b6941e60d7a7ee4
Mark Oates <markoates0@gmail.com>
2022-08-03 09:04:11 -0400

    Add "Tins3" to project nav projects

--- src/Hexagon/ProjectNavigator.cpp
+++ src/Hexagon/ProjectNavigator.cpp
@@ -84,6 +84,7 @@ bool ProjectNavigator::initialize()
       { "TINS2022-test", "/Users/markoates/Repos/TINS2022-test/" },
       { "Tins2", "/Users/markoates/Repos/Tins2/" },
       { "AllegroMembersClub", "/Users/markoates/Repos/AllegroMembersClub/" },
+      { "Tins3", "/Users/markoates/Repos/Tins3/" },
       //{ "tins2021", "/Users/markoates/Repos/tins2021/" },
    });
    main_menu.set_color(ALLEGRO_COLOR{0.8f, 0.85f, 0.86f, 0.86f});
