9b5e1b5a48c08c33fd31b9a179fe3fdbe66d21ca
Mark Oates <markoates0@gmail.com>
2022-09-04 11:51:16 -0400

    Update "build" program and add new stage

--- programs/build.cpp
+++ programs/build.cpp
@@ -38,10 +38,10 @@ class ListQuintessences : public Hexagon::BuildSystem::BuildStages::Base
       {}
       virtual ~ListQuintessences() {}
 
-      std::string build_list_quintessence_shell_command()
+      std::string build_list_quintessences_shell_command()
       {
          std::stringstream output_filename;
-         output_filename << "quintessences_" << BUILD_NUMBER << ".txt";
+         output_filename << "ListQuintessences_" << BUILD_NUMBER << ".txt";
          std::stringstream shell_command;
          shell_command << "(cd " << project_directory << " && make list_quintessences > " << output_filename.str() << ")";
          return shell_command.str();
@@ -50,7 +50,7 @@ class ListQuintessences : public Hexagon::BuildSystem::BuildStages::Base
       virtual void execute() override
       {
          if (executed) return;
-         Blast::ShellCommandExecutorWithCallback shell_command_executor(build_list_quintessence_shell_command());
+         Blast::ShellCommandExecutorWithCallback shell_command_executor(build_list_quintessences_shell_command());
          shell_command_result = shell_command_executor.execute();
          executed = true;
       }
@@ -65,20 +65,37 @@ class BuildQuintessences : public Hexagon::BuildSystem::BuildStages::Base
    private:
       std::string project_directory;
       std::string build_number;
+      std::string shell_command_result;
+      bool executed;
 
    public:
       BuildQuintessences(std::string project_directory, std::string build_number)
          : Hexagon::BuildSystem::BuildStages::Base(BuildQuintessences::TYPE)
          , project_directory(project_directory)
          , build_number(build_number)
+         , shell_command_result()
+         , executed(false)
       {}
       virtual ~BuildQuintessences() {}
+
+      std::string build_make_quintessences_shell_command()
+      {
+         std::stringstream output_filename;
+         output_filename << "BuildQuintessences_" << BUILD_NUMBER << ".txt";
+         std::stringstream shell_command;
+         shell_command << "(cd " << project_directory << " && make quintessences > " << output_filename.str() << ")";
+         return shell_command.str();
+      }
 
       virtual void execute() override
       {
          std::string filename_with_quintessences_list = project_directory + "/quintessences_" + build_number + ".txt";
          std::string contents = php::file_get_contents(filename_with_quintessences_list);
          std::vector<std::string> contents_lines = Blast::StringSplitter(contents, '\n').split();
+
+         Blast::ShellCommandExecutorWithCallback shell_command_executor(build_make_quintessences_shell_command());
+         shell_command_result = shell_command_executor.execute();
+         executed = true;
       }
 };
 
