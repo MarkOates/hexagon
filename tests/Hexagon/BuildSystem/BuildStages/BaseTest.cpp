cf5ae3864e2e193248408d7c2c40ac7c1109e2bf
Mark Oates <markoates0@gmail.com>
2022-09-04 19:20:55 -0400

    * Add tests for getting and setting started_at and ended_at

--- tests/Hexagon/BuildSystem/BuildStages/BaseTest.cpp
+++ tests/Hexagon/BuildSystem/BuildStages/BaseTest.cpp
@@ -35,6 +35,35 @@ TEST(Hexagon_BuildSystem_BuildStages_BaseTest, get_started_at__will_return_the_d
 }
 
 
+TEST(Hexagon_BuildSystem_BuildStages_BaseTest, set_started_at__will_set_the_started_at_value)
+{
+   Hexagon::BuildSystem::BuildStages::Base base;
+   std::chrono::system_clock::time_point time_point = std::chrono::system_clock::now();
+   base.set_started_at(time_point);
+   std::chrono::system_clock::time_point actual_started_at = base.get_started_at();
+   EXPECT_EQ(time_point, actual_started_at);
+}
+
+
+TEST(Hexagon_BuildSystem_BuildStages_BaseTest, get_ended_at__will_return_the_default_value)
+{
+   Hexagon::BuildSystem::BuildStages::Base base;
+   std::chrono::system_clock::time_point expected_time_point;
+   std::chrono::system_clock::time_point actual_time_point = base.get_ended_at();
+   EXPECT_EQ(expected_time_point, actual_time_point);
+}
+
+
+TEST(Hexagon_BuildSystem_BuildStages_BaseTest, set_ended_at__will_set_the_ended_at_value)
+{
+   Hexagon::BuildSystem::BuildStages::Base base;
+   std::chrono::system_clock::time_point time_point = std::chrono::system_clock::now();
+   base.set_ended_at(time_point);
+   std::chrono::system_clock::time_point actual_ended_at = base.get_ended_at();
+   EXPECT_EQ(time_point, actual_ended_at);
+}
+
+
 TEST(Hexagon_BuildSystem_BuildStages_BaseTest, derived_classes_will_have_the_expected_type)
 {
    BuildStagesBaseTestClass test_class;
