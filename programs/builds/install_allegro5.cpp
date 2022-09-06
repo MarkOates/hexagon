834a1065c0a1f5a6a10f7c4f4e1f9e26ad033bb9
Mark Oates <markoates0@gmail.com>
2022-09-06 00:13:22 -0400

    Add BuildFromSourceInTempFolder and ValidatePresenceOfBuiltExecutable

--- programs/builds/install_allegro5.cpp
+++ programs/builds/install_allegro5.cpp
@@ -172,6 +172,85 @@ public:
 
 
 
+class BuildFromSourceInTempFolder : public Hexagon::BuildSystem::BuildStages::Base
+{
+private:
+   void execute_shell_commands()
+   {
+      //TODO: require '/' character at end
+      std::stringstream shell_command;
+      shell_command << "(cd " << name_of_temp_location_to_build << " && make)";
+      std::cout << shell_command.str() << std::endl;
+      Blast::ShellCommandExecutorWithCallback shell_command_executor(shell_command.str());
+      shell_command_result = shell_command_executor.execute();
+
+      Blast::ShellCommandExecutorWithCallback shell_command_executor2("echo $?");
+      shell_command_response_code = shell_command_executor2.execute();
+   }
+
+public:
+   static constexpr char* TYPE = "BuildFromSourceInTempFolder";
+   std::string name_of_temp_location_to_build;
+   std::string shell_command_result;
+   std::string shell_command_response_code;
+
+   BuildFromSourceInTempFolder()
+      : Hexagon::BuildSystem::BuildStages::Base(TYPE)
+      , name_of_temp_location_to_build("/Users/markoates/Releases/tmp/54321-MacOS/")
+      , shell_command_result()
+      , shell_command_response_code()
+   {}
+
+   virtual bool execute() override
+   {
+      execute_shell_commands();
+      if (shell_command_response_code == "0\n") return true;
+      return false;
+   }
+};
+
+
+
+class ValidatePresenceOfBuiltExecutable : public Hexagon::BuildSystem::BuildStages::Base
+{
+private:
+   void execute_shell_commands()
+   {
+      //TODO: require '/' character at end
+      std::stringstream shell_command;
+      shell_command << "(cd " << name_of_temp_location_to_build << " && ((ls \"./" << name_of_expected_executable << "\" && echo yes) || echo no))";
+      std::cout << shell_command.str() << std::endl;
+      Blast::ShellCommandExecutorWithCallback shell_command_executor(shell_command.str());
+      shell_command_result = shell_command_executor.execute();
+
+      Blast::ShellCommandExecutorWithCallback shell_command_executor2("echo $?");
+      shell_command_response_code = shell_command_executor2.execute();
+   }
+
+public:
+   static constexpr char* TYPE = "ValidatePresenceOfBuiltExecutable";
+   std::string name_of_temp_location_to_build;
+   std::string name_of_expected_executable;
+   std::string shell_command_result;
+   std::string shell_command_response_code;
+
+   ValidatePresenceOfBuiltExecutable()
+      : Hexagon::BuildSystem::BuildStages::Base(TYPE)
+      , name_of_temp_location_to_build("/Users/markoates/Releases/tmp/54321-MacOS/")
+      , name_of_expected_executable("TheWeepingHouse")
+   {}
+
+   virtual bool execute() override
+   {
+      execute_shell_commands();
+      if (shell_command_result == ("./" + name_of_expected_executable + "\nyes\n")) return true;
+      return false;
+   }
+};
+
+
+
+
 
 
 int main(int argc, char **argv)
@@ -179,13 +258,17 @@ int main(int argc, char **argv)
    Hexagon::BuildSystem::BuildStageFactory build_stage_factory;
    Hexagon::BuildSystem::Builds::Base *build = new Hexagon::BuildSystem::Builds::Base;
    build->set_build_stages({
-      // validate these are present
-      new ValidateDylibBundlerVersion(),
-      new ValidateIconutil(),
-      new ValidateSips(),
+      //// validate these are present
+      //new ValidateDylibBundlerVersion(),
+      //new ValidateIconutil(),
+      //new ValidateSips(),
 
-      // get copy of source release
-      new CopySourceReleaseFilesForBuilding(),
+      //// get copy of source release
+      //new CopySourceReleaseFilesForBuilding(),
+
+      //// make a build from the source
+      //new BuildFromSourceInTempFolder(),
+      new ValidatePresenceOfBuiltExecutable(),
    });
    build->run();
    //parallel_build->run_all_in_parallel();
