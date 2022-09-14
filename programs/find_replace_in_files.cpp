9c8f11a98c28f292d2cfc38ba5c9bf1d4395708c
Mark Oates <markoates0@gmail.com>
2022-09-13 21:27:41 -0400

    Expand find_replace_in_files to accept 4 arguments

--- programs/find_replace_in_files.cpp
+++ programs/find_replace_in_files.cpp
@@ -19,10 +19,11 @@ std::string replace_all(std::string str, const std::string& from, const std::str
 
 int main(int argc, char **argv)
 {
-   if (argc != 4)
+   if (argc != 4 && argc != 5)
    {
       std::stringstream error_message;
-      error_message << "You must have 3 args, file_find_pattern, search_pattern, replace_pattern" << std::endl
+      error_message << "You must have 3 (or 4) args, [ file_find_pattern, search_pattern, replace_pattern ] or "
+                    << "[ file_find_pattern, file_exclude_pattern, search_pattern, replace_pattern ]" << std::endl
                     << "Example: " << argv[0] << " *.txt foo baz" << std::endl
                     << "Note this program will not run the command, it will simply generate it for you to copy "
                     << "and run yourself." << std::endl;
@@ -30,14 +31,34 @@ int main(int argc, char **argv)
       return 1;
    }
 
-   std::string file_find_pattern = argv[1];
-   std::string search_pattern = argv[2];
-   std::string replace_pattern = argv[3];
+   std::string file_find_pattern;
+   std::string exclude_file_find_pattern;
+   std::string search_pattern;
+   std::string replace_pattern;
+
+   if (argc == 4)
+   {
+      file_find_pattern = argv[1];
+      search_pattern = argv[2];
+      replace_pattern = argv[3];
+   }
+   if (argc == 5)
+   {
+      file_find_pattern = argv[1];
+      exclude_file_find_pattern = argv[2];
+      search_pattern = argv[3];
+      replace_pattern = argv[4];
+   }
 
    // sanitize the file_find_pattern
    // must escape " char
    file_find_pattern = replace_all(file_find_pattern, "\\", "\\\\");
    file_find_pattern = replace_all(file_find_pattern, "\"", "\\\"");
+
+   // sanitize the exclude_file_find_pattern
+   // must escape " char
+   exclude_file_find_pattern = replace_all(exclude_file_find_pattern, "\\", "\\\\");
+   exclude_file_find_pattern = replace_all(exclude_file_find_pattern, "\"", "\\\"");
 
    // sanitize the search_pattern
    // must escape \, ', and / char
@@ -55,6 +76,8 @@ int main(int argc, char **argv)
    // find . -type f -name "*.txt" -print0 | xargs -0 sed -i '' -e 's/foo/bar/g'
 
    // TODO
+   // currently in exclude_file_find_pattern
+   // if blank, don't include it
    // -not -path "./directory/*"
 
    std::stringstream dry_run_command;
@@ -66,9 +89,8 @@ int main(int argc, char **argv)
 
    std::cout << "====== DRY RUN =======" << std::endl;
    std::cout << dry_run_command.str();
-   std::cout << "====== DRY RUN =======" << std::endl;
+   std::cout << "====== FULL COMMAND =======" << std::endl;
    std::cout << command.str();
-   std::cout << "====== DRY RUN =======" << std::endl;
 
    return 0;
 }
