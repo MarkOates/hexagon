4f16425397a63a8135920401dc6ecf296a408426
Mark Oates <markoates0@gmail.com>
2022-12-27 18:20:31 -0500

    * Increase the number of messages to show to 7

--- src/Hexagon/ChatGPTIntegration/Chat/Stage.cpp
+++ src/Hexagon/ChatGPTIntegration/Chat/Stage.cpp
@@ -185,7 +185,7 @@ void Stage::render()
          get_place().size.x, // width
          get_place().size.y  // height
       );
-      conversation_view.set_num_messages_to_show(4);
+      conversation_view.set_num_messages_to_show(7);
       conversation_view.set_skip_empty_messages(true);
       conversation_view.render();
    }
