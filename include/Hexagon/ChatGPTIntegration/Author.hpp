6ca2a28568eec1f7b6ee307c65d8d17dfe2be57a
Mark Oates <markoates0@gmail.com>
2022-12-27 12:06:21 -0500

    * Add a #display_background_color# option to the ConversationView

--- include/Hexagon/ChatGPTIntegration/Author.hpp
+++ include/Hexagon/ChatGPTIntegration/Author.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <allegro5/allegro.h>
 #include <cstdint>
 #include <string>
 
@@ -16,22 +17,25 @@ namespace Hexagon
          std::string identifier;
          std::string display_name;
          std::string avatar_bitmap_identifier;
+         ALLEGRO_COLOR display_background_color;
 
       protected:
 
 
       public:
-         Author(uint32_t id=0, std::string identifier="[unset-identifier]", std::string display_name="[unset-name]", std::string avatar_bitmap_identifier="[unset-avatar_bitmap_identifier]");
+         Author(uint32_t id=0, std::string identifier="[unset-identifier]", std::string display_name="[unset-name]", std::string avatar_bitmap_identifier="[unset-avatar_bitmap_identifier]", ALLEGRO_COLOR display_background_color=ALLEGRO_COLOR{0.0, 0.0, 0.0, 0.0});
          ~Author();
 
          void set_id(uint32_t id);
          void set_identifier(std::string identifier);
          void set_display_name(std::string display_name);
          void set_avatar_bitmap_identifier(std::string avatar_bitmap_identifier);
+         void set_display_background_color(ALLEGRO_COLOR display_background_color);
          uint32_t get_id() const;
          std::string get_identifier() const;
          std::string get_display_name() const;
          std::string get_avatar_bitmap_identifier() const;
+         ALLEGRO_COLOR get_display_background_color() const;
       };
    }
 }
--- src/Hexagon/ChatGPTIntegration/Author.cpp
+++ src/Hexagon/ChatGPTIntegration/Author.cpp
@@ -11,11 +11,12 @@ namespace ChatGPTIntegration
 {
 
 
-Author::Author(uint32_t id, std::string identifier, std::string display_name, std::string avatar_bitmap_identifier)
+Author::Author(uint32_t id, std::string identifier, std::string display_name, std::string avatar_bitmap_identifier, ALLEGRO_COLOR display_background_color)
    : id(id)
    , identifier(identifier)
    , display_name(display_name)
    , avatar_bitmap_identifier(avatar_bitmap_identifier)
+   , display_background_color(display_background_color)
 {
 }
 
@@ -49,6 +50,12 @@ void Author::set_avatar_bitmap_identifier(std::string avatar_bitmap_identifier)
 }
 
 
+void Author::set_display_background_color(ALLEGRO_COLOR display_background_color)
+{
+   this->display_background_color = display_background_color;
+}
+
+
 uint32_t Author::get_id() const
 {
    return id;
@@ -70,6 +77,12 @@ std::string Author::get_display_name() const
 std::string Author::get_avatar_bitmap_identifier() const
 {
    return avatar_bitmap_identifier;
+}
+
+
+ALLEGRO_COLOR Author::get_display_background_color() const
+{
+   return display_background_color;
 }
 
 
