523d518e188a11922f232109139596f947bb8e06
Mark Oates <markoates0@gmail.com>
2023-08-25 12:15:00 -0400

    * Add method for passing in project name to ComponentRelationsNavigator/ComponentRelations

--- src/Hexagon/ComponentRelationsNavigator/ComponentRelations.cpp
+++ src/Hexagon/ComponentRelationsNavigator/ComponentRelations.cpp
@@ -5,6 +5,9 @@
 #include <Blast/Project/ComponentDependencyLister.hpp>
 #include <Blast/Project/ComponentRelativeLister.hpp>
 #include <Hexagon/ComponentRelationsNavigator/DocumentationDependentsJSONLoader.hpp>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
 
 
 namespace Hexagon
@@ -27,7 +30,10 @@ ComponentRelations::~ComponentRelations()
 
 std::vector<std::string> ComponentRelations::build_dependents_list()
 {
-   Hexagon::ComponentRelationsNavigator::DocumentationDependentsJSONLoader dependents_relations_loader;
+   std::string dependents_json_filename = "/Users/markoates/Repos/hexagon/documentation/dependents.json";
+   Hexagon::ComponentRelationsNavigator::DocumentationDependentsJSONLoader dependents_relations_loader(
+      dependents_json_filename
+   );
    return dependents_relations_loader.build_dependent_names_for_component_name(component.get_name());
 }
 
@@ -44,6 +50,38 @@ std::vector<std::string> ComponentRelations::build_dependencies_list()
    Blast::Project::ComponentDependencyLister dependency_lister(&component);
    std::vector<std::string> result_component_names = dependency_lister.list_component_dependency_names();
    return result_component_names;
+}
+
+std::string ComponentRelations::build_implicit_json_dependents_filename_for_project(std::string project_name)
+{
+   if (!(valid_project_name(&project_name)))
+   {
+      std::stringstream error_message;
+      error_message << "[ComponentRelations::build_implicit_json_dependents_filename_for_project]: error: guard \"valid_project_name(&project_name)\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("ComponentRelations::build_implicit_json_dependents_filename_for_project: error: guard \"valid_project_name(&project_name)\" not met");
+   }
+   return "/Users/markoates/Repos/" + project_name + "/documentation/dependents.json";
+}
+
+bool ComponentRelations::valid_project_name(std::string* project_name)
+{
+   if (!(project_name))
+   {
+      std::stringstream error_message;
+      error_message << "[ComponentRelations::valid_project_name]: error: guard \"project_name\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("ComponentRelations::valid_project_name: error: guard \"project_name\" not met");
+   }
+   if (project_name->empty()) return false;
+   for (char c : *project_name)
+   {
+      if (!std::isalnum(c) && c != '_' && c != '-')
+      {
+         return false;
+      }
+   }
+   return true;
 }
 
 
