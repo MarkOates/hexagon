67cb2f61ae988529495fa0c363fb36d93f0ebc19
Mark Oates <markoates0@gmail.com>
2023-10-21 13:09:24 -0400

    * Use Hexagon/DependencyManager to provide dependencies to ComponentRelations

--- tests/Hexagon/ComponentRelationsNavigator/ComponentRelationsTest.cpp
+++ tests/Hexagon/ComponentRelationsNavigator/ComponentRelationsTest.cpp
@@ -23,3 +23,25 @@ TEST(Hexagon_ComponentRelationsNavigator_ComponentRelationsTest, build_dependent
 }
 
 
+TEST(Hexagon_ComponentRelationsNavigator_ComponentRelationsTest, build_dependencies__returns_the_class_dependencies)
+{
+   //Blast::Project::Component component("AllegroFlare/FontBin", "allegro_flare");
+   Blast::Project::Component component("Hexagon/PacketRenderer", "/Users/markoates/Repos/hexagon");
+   Hexagon::ComponentRelationsNavigator::ComponentRelations component_relations(component);
+
+   std::vector<std::string> expected_component_dependencies_names = {
+      "AllegroFlare/Bin",
+      "AllegroFlare/ConsoleColor",
+      "AllegroFlare/FontBin",
+      "Hexagon/Elements/Window",
+      "Hexagon/Packet",
+      "allegro_flare/placement2d",
+      "allegro_flare/vec2b",
+      "allegro_flare/vec2d",
+   };
+   std::vector<std::string> actual_component_dependencies_names = component_relations.build_dependencies_list();
+
+   EXPECT_EQ(expected_component_dependencies_names, actual_component_dependencies_names);
+}
+
+
